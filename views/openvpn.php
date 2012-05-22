8ca131daff60f056f64872ab468e532b9fc727de
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-05-22 16:44:49 +0000

    - Added forgotten file on last checkin

---
+++ views/openvpn.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * User OpenVPN configuration view.
+ *
+ * @category   ClearOS
+ * @package    User_Certificates
+ * @subpackage Views
+ * @author     ClearFoundation <developer@clearfoundation.com>
+ * @copyright  2012 ClearFoundation
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       http://www.clearfoundation.com/docs/developer/apps/user_certificates/
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+//  
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Load dependencies
+///////////////////////////////////////////////////////////////////////////////
+
+$this->lang->load('user_certificates');
+$this->lang->load('openvpn');
+$this->lang->load('base');
+
+///////////////////////////////////////////////////////////////////////////////
+// Form
+///////////////////////////////////////////////////////////////////////////////
+
+echo form_open('user_certificates/openvpn');
+echo form_header(lang('openvpn_app_name'));
+
+echo field_dropdown('configuration', $configurations, $configuration, lang('user_certificates_configuration_file'));
+
+echo field_button_set(
+    array(form_submit_custom('submit-openvpn', lang('base_download')))
+);
+
+echo form_footer();
+echo form_close();
