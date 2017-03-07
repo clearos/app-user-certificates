4d20d25dad2a49d9ffe6f79726f4a3a3ddafbb8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'பாதுகாப்பு சான்றிதழ் நீங்கள் நாள் அடிப்படையில் ஒரு நாளில் பயன்படுத்த என்று பல்வேறு பயன்பாடுகளை பாதுகாக்க பயன்படுத்தப்படுகின்றன.';
+$lang['user_certificates_app_name'] = 'பயனர் சான்றிதழ்கள்';
+$lang['user_certificates_certificates'] = 'சான்றிதழ்கள்';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'பயனர் சான்றிதழ் இந்த கணக்கு கிடைக்கவில்லை.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'மன்னிக்கவும், பாதுகாப்பு சான்றிதழ்களை மட்டும் இந்த கணினியில் இருக்கின்றன. மேலும் தகவலுக்கு உங்கள் கணினி நிர்வாகியை தொடர்பு கொள்ளவும்.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'மன்னிக்கவும், பாதுகாப்பு சான்றிதழ்களை உங்கள் கணக்கு இயக்கப்படவில்லை.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'மன்னிக்கவும், பாதுகாப்பு சான்றிதழ்களை அமைப்பு இன்னும் துவக்க முடியவில்லை. மேலும் தகவலுக்கு உங்கள் கணினி நிர்வாகியை தொடர்பு கொள்ளவும்.';
+$lang['user_certificates_configuration_file'] = 'கட்டமைப்பு கோப்பு';
+$lang['user_certificates_create_certificates'] = 'சான்றிதழ்கள் உருவாக்கு';
+$lang['user_certificates_initialize_certificate_help'] = 'உங்கள் பாதுகாப்பு சான்றிதழ்களை பாதுகாக்கும் ஒரு கடவுச்சொல்லை குறிப்பிடவும். நீங்கள் கடவுச்சொல்லை மறந்து விட்டால், நீங்கள் சான்றிதழ் ஒரு புதிய தொகுப்பு மேல் தொடங்க வேண்டும்.';
