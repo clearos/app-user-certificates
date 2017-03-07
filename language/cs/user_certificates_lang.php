c64383ff313f5acd206b1b70883fe0879b6f4c99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:44:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Bezpečnostní certifikáty slouží k zajištění různé aplikace, které používáte na každodenní bázi.';
+$lang['user_certificates_app_name'] = 'Uživatelské certifikáty';
+$lang['user_certificates_certificates'] = 'Certifikáty';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Uživatelské certifikáty nejsou k dispozici pro tento účet.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Je nám líto, bezpečnostní certifikáty nejsou k dispozici v tomto systému. Obraťte se na správce systému pro více informací.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Je nám líto, bezpečnostní certifikáty nejsou povoleny pro váš účet.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Je nám líto, nebyl dosud inicializován systém bezpečnostní certifikáty. Obraťte se na správce systému pro více informací.';
+$lang['user_certificates_configuration_file'] = 'konfigurační soubor';
+$lang['user_certificates_create_certificates'] = 'Vytvořit Certifikáty';
+$lang['user_certificates_initialize_certificate_help'] = 'Zadejte heslo pro ochranu bezpečnostních certifikátů. Pokud zapomenete heslo, budete muset začít znovu s novou sadou certifikátů.';
