56f9f0adc383d6339affeba1e6e003d0fc1d2a49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Security sertipiko ay ginagamit upang ma-secure ang iba&#39;t ibang apps na ginagamit mo sa isang araw-araw na batayan.';
+$lang['user_certificates_app_name'] = 'Certificates User';
+$lang['user_certificates_certificates'] = 'Certificates';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Sertipiko ng gumagamit ay hindi magagamit para sa account na ito.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Paumanhin, sertipiko ng seguridad ay hindi magagamit sa system na ito. Mangyaring makipag-ugnay sa iyong system administrator para sa higit pang impormasyon.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Paumanhin, sertipiko ng seguridad ay hindi pinagana para sa iyong account.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Paumanhin, ang mga sertipiko ng seguridad na sistema ay hindi pa nasimulan. Mangyaring makipag-ugnay sa iyong system administrator para sa higit pang impormasyon.';
+$lang['user_certificates_configuration_file'] = 'configuration File';
+$lang['user_certificates_create_certificates'] = 'lumikha Certificates';
+$lang['user_certificates_initialize_certificate_help'] = 'Mangyaring tukuyin ang isang password para sa pagprotekta ng iyong mga sertipiko ng seguridad. Kung nakalimutan mo ang password, kakailanganin mo upang magsimula sa isang bagong hanay ng mga sertipiko.';
