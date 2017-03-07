75ec2ef4f66b26bfce8f7bae3c7a3c58c2ab59ba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'گواهینامه های امنیتی استفاده می شود برای حفظ برنامه های مختلف که شما در یک روز به صورت روز استفاده کنید.';
+$lang['user_certificates_app_name'] = 'گواهینامه کاربر';
+$lang['user_certificates_certificates'] = 'گواهینامه ها';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'گواهی کاربر در دسترس نیست برای این حساب.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'با عرض پوزش، گواهینامه های امنیتی موجود در این سیستم است. لطفا با مدیر سیستم خود را برای اطلاعات بیشتر تماس بگیرید.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'با عرض پوزش، گواهینامه های امنیتی برای حساب کاربری شما فعال نیست.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'با عرض پوزش، سیستم گواهینامه های امنیتی هنوز مقداردهی اولیه نشده است. لطفا با مدیر سیستم خود را برای اطلاعات بیشتر تماس بگیرید.';
+$lang['user_certificates_configuration_file'] = 'فایل پیکربندی';
+$lang['user_certificates_create_certificates'] = 'درست گواهینامه';
+$lang['user_certificates_initialize_certificate_help'] = 'لطفا یک رمز عبور برای حفاظت از گواهینامه های امنیتی خود را مشخص کنید. اگر کلمه عبور را فراموش کرده اید، شما نیاز به بیش از شروع با یک مجموعه جدید از گواهی.';
