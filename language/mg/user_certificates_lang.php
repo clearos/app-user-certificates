1c51d13bce8666ef591be9b2ac4ae2c86713fa35
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Security taratasy fanamarinana no ampiasaina mba hahazoana isan-karazany fampiharana izay ampiasaina amin&#39;ny isan&#39;andro fototra.';
+$lang['user_certificates_app_name'] = 'User Certificates';
+$lang['user_certificates_certificates'] = 'taratasy fanamarinana';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'User taratasy fanamarinana dia tsy misy noho io tantara io.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Miala tsiny, fiarovana taratasy fanamarinana dia tsy hita ao amin&#39;ny tontolo ity. Mba andefaso olona ny rafitra mpitantana raha mila fanazavana fanampiny.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Miala tsiny, fiarovana taratasy fanamarinana dia tsy afaka noho ny tantara.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Miala tsiny, ny fiarovana rafitra taratasy fanamarinana dia tsy mbola initialized. Mba andefaso olona ny rafitra mpitantana raha mila fanazavana fanampiny.';
+$lang['user_certificates_configuration_file'] = 'Configuration File';
+$lang['user_certificates_create_certificates'] = 'hamorona Certificates';
+$lang['user_certificates_initialize_certificate_help'] = 'Azafady mamaritra ny tenimiafina ho an&#39;ny fiarovana ny fiarovana fanamarinana. Raha manadino ny tenimiafina, dia mila mamorona hafa vaovao indray amin&#39;ny vaovao napetraka ny mari-pankasitrahana.';
