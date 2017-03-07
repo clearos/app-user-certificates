ee325caa1f2aca96cb918d9346d4007a1b97f786
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:46:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Biztonsági tanúsítványok rögzítésére használt különböző alkalmazásokat, hogy használja a napi alapon.';
+$lang['user_certificates_app_name'] = 'felhasználói tanúsítványok';
+$lang['user_certificates_certificates'] = 'tanúsítványok';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Felhasználói tanúsítvány nem áll rendelkezésre erre a számlára.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Sajnos a biztonsági tanúsítvány nem áll rendelkezésre ezen a rendszeren. Kérjük, forduljon a rendszergazdához további információkért.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Sajnos a biztonsági tanúsítványok nem engedélyezve a fiókjában.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Sajnos a biztonsági tanúsítványok rendszere még nem inicializált. Kérjük, forduljon a rendszergazdához további információkért.';
+$lang['user_certificates_configuration_file'] = 'konfigurációs fájl';
+$lang['user_certificates_create_certificates'] = 'Készítsen tanúsítványok';
+$lang['user_certificates_initialize_certificate_help'] = 'Kérjük, adjon meg egy jelszót védi a biztonsági tanúsítványok. Ha elfelejtette a jelszavát, akkor meg kell kezdeni egy új igazolások.';
