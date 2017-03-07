3e257980edfb17ac894922502ddcaf2030649833
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:41:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Təhlükəsizlik sertifikatları siz gün əsasında bir gün istifadə müxtəlif apps təmin etmək üçün istifadə olunur.';
+$lang['user_certificates_app_name'] = 'İstifadəçi Sertifikatlar';
+$lang['user_certificates_certificates'] = 'Sertifikatlar';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'İstifadəçi sertifikatları bu haqq-hesab üçün mövcud deyil.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Bağışlayın, təhlükəsizlik sertifikatları bu sistem mövcud deyil. Daha ətraflı məlumat üçün sistem administratoru ilə əlaqə saxlayın.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Bağışlayın, təhlükəsizlik sertifikatları hesabınız üçün effektiv deyil.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Bağışlayın, təhlükəsizlik sertifikatları sistemi hələ başlatılmadı deyil. Daha ətraflı məlumat üçün sistem administratoru ilə əlaqə saxlayın.';
+$lang['user_certificates_configuration_file'] = 'Configuration Fayl';
+$lang['user_certificates_create_certificates'] = 'Sertifikatlar yaradın';
+$lang['user_certificates_initialize_certificate_help'] = 'təhlükəsizlik sertifikatları qorumaq üçün bir parol daxil edin. Siz parol unutsanız, sertifikatlar bir yeni dəsti ilə başlamaq lazımdır.';
