258f7b1bd6f3c4910bbfc6c010b64c8983ea7042
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Security asambodo na-eji nọrọ ná nchebe dị iche iche ngwa ọdịnala na ị na-eji on a n&#39;ụbọchị ruo n&#39;ụbọchị ndabere.';
+$lang['user_certificates_app_name'] = 'User Asambodo';
+$lang['user_certificates_certificates'] = 'Asambodo';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Onye ọrụ asambodo na-adịghị n&#39;ihi na a na akaụntụ.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Ndo, nche asambodo ndị a adịghị na usoro. Biko kpọtụrụ gị usoro nchịkwa maka ozi ndị ọzọ.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Ndo, nche asambodo na-adịghị nyeere n&#39;ihi na akaụntụ gị.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Ndo, na obi iru ala asambodo usoro bụghị ma e initialized. Biko kpọtụrụ gị usoro nchịkwa maka ozi ndị ọzọ.';
+$lang['user_certificates_configuration_file'] = 'nhazi Njikwa';
+$lang['user_certificates_create_certificates'] = 'Mepụta Asambodo';
+$lang['user_certificates_initialize_certificate_help'] = 'Biko, dee a paswọọdụ maka echebe gị nche asambodo. Ọ bụrụ na ị echefu paswọọdụ, ị ga-mkpa-amalite n&#39;elu na a ọhụrụ set nke asambodo.';
