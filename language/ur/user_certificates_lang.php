96bdffb4380f979c7afda13e1ce559e1489fb891
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'سیکورٹی سرٹیفکیٹ آپ دن کی بنیاد پر ایک دن کے استعمال کرنے والے مختلف اطلاقات کو محفوظ کرنے کے استعمال کیا جاتا ہے.';
+$lang['user_certificates_app_name'] = 'یوزر سرٹیفکیٹس';
+$lang['user_certificates_certificates'] = 'سرٹیفکیٹس';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'یوزر کے سرٹیفیکیٹ اس اکاؤنٹ کے لئے دستیاب نہیں ہیں.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'معذرت، سیکورٹی سرٹیفکیٹ اس نظام پر دستیاب نہیں ہیں. مزید معلومات کے لئے آپ کے سسٹم ایڈمنسٹریٹر سے رابطہ کریں.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'معذرت، سیکورٹی سرٹیفکیٹ آپ کے اکاؤنٹ کیلئے فعال نہیں ہیں.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'معذرت، سیکورٹی سرٹیفکیٹ نظام ابھی تک initialized نہیں کی گئی ہے. مزید معلومات کے لئے آپ کے سسٹم ایڈمنسٹریٹر سے رابطہ کریں.';
+$lang['user_certificates_configuration_file'] = 'ترتیب دینے کی فائل';
+$lang['user_certificates_create_certificates'] = 'سرٹیفکیٹس بنائیں';
+$lang['user_certificates_initialize_certificate_help'] = 'آپ کی سیکورٹی کے سرٹیفکیٹ کی حفاظت کے لئے ایک پاس ورڈ مختص کیجیے. آپ کا پاس ورڈ بھول جاتے ہیں تو، آپ کے سرٹیفکیٹ کا ایک نیا سیٹ کے ساتھ شروع کرنے کے لئے کی ضرورت ہو گی.';
---
+++ language/uz/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Xavfsizlik sertifikatlari Agar kun asosida bir kuni foydalanish turli ilovalarni ta&#39;minlash uchun ishlatiladi.';
+$lang['user_certificates_app_name'] = 'foydalanuvchi sertifikatlari';
+$lang['user_certificates_certificates'] = 'Sertifikatlar';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Foydalanuvchi sertifikatlari, bu hisob uchun foydalanish mumkin emas.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Kechirasiz, xavfsizlik sertifikatlari bu tizimda mavjud emas. Qo&#39;shimcha ma&#39;lumot olish uchun tizim boshqaruvchisiga murojaat qiling.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Kechirasiz, xavfsizlik sertifikatlari hisobingizga uchun yoqadi emas.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Kechirasiz, xavfsizlik sertifikatlari tizimi hali başlatılmamış. Qo&#39;shimcha ma&#39;lumot olish uchun tizim boshqaruvchisiga murojaat qiling.';
+$lang['user_certificates_configuration_file'] = 'konfiguratsiya Fayl';
+$lang['user_certificates_create_certificates'] = 'sertifikatlari yaratish';
+$lang['user_certificates_initialize_certificate_help'] = 'Sizning xavfsizlik sertifikatlari himoya qilish uchun parol ko&#39;rsating. Agar parolni unutib qo&#39;ysangiz, sertifikatlar yangi majmui bilan ustidan boshlash kerak bo&#39;ladi.';
