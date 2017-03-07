e47067182c5ff0f61cf60b2ac26951c69befb72b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Los certificados de seguridad se utilizan para asegurar diversas aplicaciones que se utilizan en el día a día.';
+$lang['user_certificates_app_name'] = 'Los certificados de usuario';
+$lang['user_certificates_certificates'] = 'certificados';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Los certificados de usuario no están disponibles para esta cuenta.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Lo sentimos, certificados de seguridad no están disponibles en este sistema. Por favor, póngase en contacto con el administrador del sistema para obtener más información.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Lo sentimos, certificados de seguridad no están habilitadas para su cuenta.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Lo sentimos, el sistema de certificados de seguridad todavía no se ha inicializado. Por favor, póngase en contacto con el administrador del sistema para obtener más información.';
+$lang['user_certificates_configuration_file'] = 'Archivo de configuración';
+$lang['user_certificates_create_certificates'] = 'crear Certificados';
+$lang['user_certificates_initialize_certificate_help'] = 'Por favor especificar una contraseña para proteger sus certificados de seguridad. Si olvida la contraseña, tendrá que empezar de nuevo con un nuevo conjunto de certificados.';
