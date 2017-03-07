943e59bbb09c89ff32c6d55adbfd4b1db66e9891
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'tystysgrifau diogelwch yn cael eu defnyddio i sicrhau amryw o apps eich bod yn defnyddio o ddydd i ddydd.';
+$lang['user_certificates_app_name'] = 'Tystysgrifau defnyddiwr';
+$lang['user_certificates_certificates'] = 'tystysgrifau';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Nid yw tystysgrifau Defnyddiwr ar gael ar gyfer y cyfrif hwn.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Mae&#39;n ddrwg gennym, nid yw tystysgrifau diogelwch ar gael ar y system hon. Cysylltwch â&#39;ch gweinyddwr system i gael rhagor o wybodaeth.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Mae&#39;n ddrwg gennym, nid yw tystysgrifau diogelwch yn cael eu galluogi ar gyfer eich cyfrif.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Mae&#39;n ddrwg gennym, nid yw&#39;r system tystysgrifau diogelwch wedi&#39;i ymgychwyn eto. Cysylltwch â&#39;ch gweinyddwr system i gael rhagor o wybodaeth.';
+$lang['user_certificates_configuration_file'] = 'Ffeil configuration';
+$lang['user_certificates_create_certificates'] = 'creu Tystysgrifau';
+$lang['user_certificates_initialize_certificate_help'] = 'Nodwch gyfrinair ar gyfer diogelu eich tystysgrifau diogelwch. Os byddwch yn anghofio y cyfrinair, bydd angen i chi ddechrau drosodd gyda set newydd o dystysgrifau.';
---
+++ language/vi/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'chứng chỉ bảo mật được dùng để bảo đảm các ứng dụng khác nhau mà bạn sử dụng trên một ngày để cơ sở ngày.';
+$lang['user_certificates_app_name'] = 'Giấy chứng nhận sử dụng';
+$lang['user_certificates_certificates'] = 'Giấy chứng nhận';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Chứng chỉ không có sẵn cho tài khoản này.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Xin lỗi, chứng chỉ bảo mật không có sẵn trên hệ thống này. Hãy liên hệ với quản trị viên hệ thống của bạn để biết thêm thông tin.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Xin lỗi, chứng chỉ bảo mật không được kích hoạt cho tài khoản của bạn.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Xin lỗi, hệ thống chứng chỉ bảo mật vẫn chưa được khởi tạo. Hãy liên hệ với quản trị viên hệ thống của bạn để biết thêm thông tin.';
+$lang['user_certificates_configuration_file'] = 'Tập tin cấu hình';
+$lang['user_certificates_create_certificates'] = 'tạo Certificates';
+$lang['user_certificates_initialize_certificate_help'] = 'Hãy xác định một mật khẩu để bảo vệ các chứng chỉ bảo mật của bạn. Nếu bạn quên mật khẩu, bạn sẽ cần phải bắt đầu lại với một tập mới của giấy chứng nhận.';
