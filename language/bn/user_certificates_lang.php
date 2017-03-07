e6de94562da165bea26af07a5972c8406e58293f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:42:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'সিকিউরিটি সার্টিফিকেট আপনি দিনের ভিত্তিতে একটি দিনে ব্যবহার বিভিন্ন অ্যাপস নিরাপদ করার জন্য ব্যবহৃত হয়.';
+$lang['user_certificates_app_name'] = 'ব্যবহারকারী সার্টিফিকেট';
+$lang['user_certificates_certificates'] = 'সার্টিফিকেট';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ব্যবহারকারী সার্টিফিকেট এই অ্যাকাউন্টের জন্য উপলব্ধ করা হয় না.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'দুঃখিত, নিরাপত্তা শংসাপত্রগুলি এই সিস্টেমে উপলব্ধ করা হয় না. আরও তথ্যের জন্য আপনার সিস্টেম অ্যাডমিনস্ট্রেটরের সঙ্গে যোগাযোগ করুন.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'দুঃখিত, নিরাপত্তা শংসাপত্রগুলি আপনার অ্যাকাউন্টের জন্য সক্রিয় করা নেই.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'দুঃখিত, নিরাপত্তা সার্টিফিকেট সিস্টেম এখনও সক্রিয়া করা হয়েছে. আরও তথ্যের জন্য আপনার সিস্টেম অ্যাডমিনস্ট্রেটরের সঙ্গে যোগাযোগ করুন.';
+$lang['user_certificates_configuration_file'] = 'কনফিগারেশন ফাইল';
+$lang['user_certificates_create_certificates'] = 'সার্টিফিকেট তৈরি করুন';
+$lang['user_certificates_initialize_certificate_help'] = 'আপনার নিরাপত্তা শংসাপত্রগুলি রক্ষা করার জন্য একটি পাসওয়ার্ড উল্লেখ করুন. আপনি পাসওয়ার্ড ভুলে যান তাহলে, আপনি সার্টিফিকেট নতুন সেট সঙ্গে নতুন করে শুরু করতে হবে.';
