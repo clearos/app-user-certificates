a87d0efb041e8f2bdcefa97038c47f4a8cf4d968
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:40:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Sekuriteit sertifikate word gebruik om verskeie programme wat jy gebruik op &#39;n dag tot dag basis te verseker.';
+$lang['user_certificates_app_name'] = 'gebruikers Sertifikate';
+$lang['user_certificates_certificates'] = 'sertifikate';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Gebruikers sertifikate is nie beskikbaar vir hierdie rekening.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Jammer, sekuriteit sertifikate is nie op hierdie stelsel. Kontak asseblief u stelsel administrateur vir meer inligting.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Jammer, sekuriteit sertifikate is nie aangeskakel vir jou rekening.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Jammer, die sekuriteit sertifikate stelsel is nog nie geïnisialiseer. Kontak asseblief u stelsel administrateur vir meer inligting.';
+$lang['user_certificates_configuration_file'] = 'konfigurasielêer';
+$lang['user_certificates_create_certificates'] = 'Skep Sertifikate';
+$lang['user_certificates_initialize_certificate_help'] = 'Spesifiseer asseblief &#39;n wagwoord vir die beskerming van jou eie veiligheid sertifikate. As jy die wagwoord vergeet, sal jy nodig het om oor te begin met &#39;n nuwe stel van sertifikate.';
