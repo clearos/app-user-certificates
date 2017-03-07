509d41f8f9efef15d68f808538ce0226d934070e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:44:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Sikkerhed certifikater bruges til at sikre forskellige apps, som du bruger på en dag til dag basis.';
+$lang['user_certificates_app_name'] = 'Bruger-certifikater';
+$lang['user_certificates_certificates'] = 'certifikater';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Brugercertifikater er ikke tilgængelige for denne konto.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Beklager, sikkerhedscertifikater er ikke tilgængelige på dette system. Kontakt din systemadministrator for mere information.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Beklager, sikkerhedscertifikater er ikke aktiveret for din konto.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Beklager, det sikkerhedscertifikater systemet endnu ikke er blevet initialiseret. Kontakt din systemadministrator for mere information.';
+$lang['user_certificates_configuration_file'] = 'konfigurationsfil';
+$lang['user_certificates_create_certificates'] = 'Opret certifikater';
+$lang['user_certificates_initialize_certificate_help'] = 'Angiv en adgangskode til at beskytte dine sikkerhedscertifikater. Hvis du glemmer adgangskoden, skal du starte forfra med et nyt sæt af certifikater.';
