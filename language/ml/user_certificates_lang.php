ad5e2379fd9a553d35bf694ec48aebcecd3605b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'സുരക്ഷാ സർട്ടിഫിക്കറ്റുകൾ നിങ്ങൾക്ക് ഒരു നിത്യേന ഉപയോഗിക്കുന്ന വിവിധ അപ്ലിക്കേഷനുകൾ സുരക്ഷിതമാക്കാൻ ഉപയോഗിക്കുന്നു.';
+$lang['user_certificates_app_name'] = 'ഉപയോക്താവിന്റെ സർട്ടിഫിക്കറ്റുകൾ';
+$lang['user_certificates_certificates'] = 'സർട്ടിഫിക്കറ്റുകൾ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ഉപയോക്തൃ സർട്ടിഫിക്കറ്റുകൾ ഈ അക്കൗണ്ടിന് ലഭ്യമല്ല.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ക്ഷമിക്കണം, സുരക്ഷാ സർട്ടിഫിക്കറ്റുകൾ ഈ സിസ്റ്റത്തിൽ ലഭ്യമല്ല. കൂടുതൽ വിവരങ്ങൾക്ക് നിങ്ങളുടെ സിസ്റ്റം അഡ്മിനിസ്ട്രേറ്ററെ ബന്ധപ്പെടുക.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ക്ഷമിക്കണം, സുരക്ഷാ സർട്ടിഫിക്കറ്റുകൾ നിങ്ങളുടെ അക്കൗണ്ട് പ്രവർത്തനക്ഷമമാക്കിയിട്ടില്ല.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ക്ഷമിക്കണം, സുരക്ഷാ സർട്ടിഫിക്കറ്റുകൾ സിസ്റ്റം ഇതുവരെ ആരംഭിച്ചു ചെയ്തിട്ടില്ല. കൂടുതൽ വിവരങ്ങൾക്ക് നിങ്ങളുടെ സിസ്റ്റം അഡ്മിനിസ്ട്രേറ്ററെ ബന്ധപ്പെടുക.';
+$lang['user_certificates_configuration_file'] = 'കോൺഫിഗറേഷൻ ഫയൽ';
+$lang['user_certificates_create_certificates'] = 'സർട്ടിഫിക്കറ്റുകൾ സൃഷ്ടിക്കുക';
+$lang['user_certificates_initialize_certificate_help'] = 'ദയവായി നിങ്ങളുടെ സുരക്ഷ സർട്ടിഫിക്കറ്റുകൾ സംരക്ഷിക്കുന്നതിനും ഒരു പാസ്വേഡ് വ്യക്തമാക്കുക. നിങ്ങൾ പാസ്വേഡ് മറന്നുപോയാൽ സർട്ടിഫിക്കറ്റുകൾ ഒരു പുതിയ കൂട്ടം ആരംഭിക്കണമെന്നും ആവശ്യമാണ്.';
