97370f73e787949ef4360b7852c9abb48020cc72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:46:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'सुरक्षा प्रमाण पत्र है कि आप दिन के आधार पर एक दिन का उपयोग विभिन्न क्षुधा सुरक्षित करने के लिए उपयोग किया जाता है।';
+$lang['user_certificates_app_name'] = 'उपयोगकर्ता प्रमाण पत्र';
+$lang['user_certificates_certificates'] = 'प्रमाण पत्र';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'उपयोगकर्ता प्रमाण पत्र इस खाते के लिए उपलब्ध नहीं हैं।';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'क्षमा करें, सुरक्षा प्रमाण पत्र इस सिस्टम पर उपलब्ध नहीं हैं। अधिक जानकारी के लिए अपने सिस्टम व्यवस्थापक से संपर्क करें।';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'क्षमा करें, सुरक्षा प्रमाण पत्र अपने खाते के लिए सक्षम नहीं हैं।';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'क्षमा करें, सुरक्षा प्रमाण पत्र प्रणाली अभी तक प्रारंभ नहीं किया गया है। अधिक जानकारी के लिए अपने सिस्टम व्यवस्थापक से संपर्क करें।';
+$lang['user_certificates_configuration_file'] = 'विन्यास फाइल';
+$lang['user_certificates_create_certificates'] = 'प्रमाण पत्र बनाने के लिए';
+$lang['user_certificates_initialize_certificate_help'] = 'अपनी सुरक्षा प्रमाण-पत्र की रक्षा करने के लिए एक पासवर्ड निर्दिष्ट करें। आप पासवर्ड भूल जाते हैं, तो आप प्रमाण पत्र का एक नया सेट के साथ शुरू करने की आवश्यकता होगी।';
