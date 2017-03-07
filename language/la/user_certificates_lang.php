39c2405eb6099bd6385a511912f412e74e4d1ce2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Testimonium Security sunt, ut esset diversis apps ut vos utor in a cotidie basis.';
+$lang['user_certificates_app_name'] = 'User Testimonia';
+$lang['user_certificates_certificates'] = 'Testimonia';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Disputatio hoc testimonium non competunt.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Paenitet, satis hoc testimonium non sit ratio. Contact vestri ratio Administratorem pro magis notitia.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Ignosce, pro tua confortavit non satis testimonium.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Sorry, et testimonium veritatis ratio, non tamen initialized. Contact vestri ratio Administratorem pro magis notitia.';
+$lang['user_certificates_configuration_file'] = 'configuration File:';
+$lang['user_certificates_create_certificates'] = 'Create Testimonia';
+$lang['user_certificates_initialize_certificate_help'] = 'Quaeso a password tutela securum testimonium. Si tu oblitus ignoro, vos mos postulo ut satus cum libellorum creauit.';
