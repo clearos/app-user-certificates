39a10f3502e2747cabe7882ada1d4c1aa0d68061
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Қауіпсіздік сертификаттар Сіз күнделікті негізде пайдалануға түрлі қолданбаларды қамтамасыз ету үшін пайдаланылады.';
+$lang['user_certificates_app_name'] = 'пайдаланушы сертификаттары';
+$lang['user_certificates_certificates'] = 'Сертификаттар';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Пайдаланушы сертификаттар осы тіркелгі үшін қол жетімді емес.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Кешіріңіз, қауіпсіздік сертификаттары бұл жүйеде қол жетімді емес. Қосымша ақпарат алу үшін жүйе әкімшісіне хабарласыңыз.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Кешіріңіз, қауіпсіздік сертификаттары тіркелгіңіз үшін қосылған жоқ.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Кешіріңіз, қауіпсіздік сертификаттары жүйесі әлі бапталғаннан жоқ. Қосымша ақпарат алу үшін жүйе әкімшісіне хабарласыңыз.';
+$lang['user_certificates_configuration_file'] = 'конфигурация файлын';
+$lang['user_certificates_create_certificates'] = 'Сертификаттар жасау';
+$lang['user_certificates_initialize_certificate_help'] = 'Егер қауіпсіздік сертификаттар қорғау үшін құпия сөзді көрсетіңіз. Егер құпия сөзді ұмытып қалсаңыз, сертификаттардың жаңа жиынтығымен астам бастау үшін қажет болады.';
