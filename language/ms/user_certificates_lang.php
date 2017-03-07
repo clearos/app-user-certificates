583aaeafe2423bd36e41bfaf895cb7bc1abce631
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'sijil keselamatan yang digunakan untuk mendapatkan pelbagai aplikasi yang anda gunakan pada hari ke hari.';
+$lang['user_certificates_app_name'] = 'Sijil pengguna';
+$lang['user_certificates_certificates'] = 'sijil';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Sijil pengguna tidak tersedia untuk akaun ini.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Maaf, sijil keselamatan tidak terdapat pada sistem ini. Sila hubungi pentadbir sistem anda untuk maklumat lanjut.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Maaf, sijil keselamatan tidak didayakan untuk akaun anda.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Maaf, sistem sijil keselamatan masih belum dimulakan. Sila hubungi pentadbir sistem anda untuk maklumat lanjut.';
+$lang['user_certificates_configuration_file'] = 'Fail konfigurasi';
+$lang['user_certificates_create_certificates'] = 'Buat Sijil';
+$lang['user_certificates_initialize_certificate_help'] = 'Sila nyatakan kata laluan untuk melindungi sijil keselamatan anda. Jika anda lupa kata laluan, anda akan perlu untuk memulakan dengan satu set baru sijil.';
