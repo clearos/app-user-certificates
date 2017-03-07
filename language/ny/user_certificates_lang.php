34f91ee7cdeaf88e2a85b782e878ea83d2a62694
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:43:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'zikalata Security ntchito kukapempha mapulogalamu osiyanasiyana kuti ntchito pa tsiku ndi tsiku maziko.';
+$lang['user_certificates_app_name'] = 'Zikalata wosuta';
+$lang['user_certificates_certificates'] = 'zikalata';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'zikalata wosuta palibe nkhani imeneyi.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Pepani, zikalata chitetezo palibe dongosolo. Funsani woyang&#39;anira dongosolo kuti mumve zambiri.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Pepani, zikalata chitetezo si zinathandiza kuti nkhani yanu.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Pepani, dongosolo zikalata chitetezo silinakwaniritsidwe initialized. Funsani woyang&#39;anira dongosolo kuti mumve zambiri.';
+$lang['user_certificates_configuration_file'] = 'kasinthidwe failo';
+$lang['user_certificates_create_certificates'] = 'Pangani Zikalata';
+$lang['user_certificates_initialize_certificate_help'] = 'Chonde mwatchutchutchu achinsinsi kuteteza zikalata zanu chitetezo. Ngati inu kuiwala achinsinsi, muyenera kuyamba ndi atsopano zikalata.';
