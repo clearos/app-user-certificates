1bff673ae4f7752f3693737c4ca53c2b3c62e0f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:45:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Sekureco atestilojn kutimas sekurigi plurajn apps ke vi uzas iun tagon al tago bazo.';
+$lang['user_certificates_app_name'] = 'uzanto registritaj';
+$lang['user_certificates_certificates'] = 'atestiloj';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Uzanto atestiloj ne estas havebla por tiu konto.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Pardonu, sekureco atestoj estas ne havebla sur tiu sistemo. Bonvolu kontakti la sistemestron por pli informo.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Pardonu, sekureco atestilojn ne ebligita por via konto.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Pardonu, la sekureco atestilojn sistemo ankoraŭ ne estis inicializado. Bonvolu kontakti la sistemestron por pli informo.';
+$lang['user_certificates_configuration_file'] = 'agordo Dosiero';
+$lang['user_certificates_create_certificates'] = 'krei registritaj';
+$lang['user_certificates_initialize_certificate_help'] = 'Bonvolu entajpi la pasvorton por protekti via sekureco atestilojn. Se vi forgesos la pasvorton, vi devas komenci super kun nova aro de atestiloj.';
