848387dd80c9bee90280cefbe47526669dd77e51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:41:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Segurtasun ziurtagiriak egunez egun bat erabili ohi dituzun hainbat apps ziurtatzeko erabiltzen dira.';
+$lang['user_certificates_app_name'] = 'Erabiltzaile Ziurtagiriak';
+$lang['user_certificates_certificates'] = 'Ziurtagiriak';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Erabiltzaile-ziurtagiriak ez dira erabilgarri kontu honetan.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Barkatu, segurtasun-ziurtagiriak ez dira sistema honetan eskuragarri. Jarri harremanetan sistemako administratzailearekin informazio gehiago eskuratzeko.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Barkatu, segurtasun ziurtagiriak ez dira zure kontua gaituta.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Barkatu, segurtasun ziurtagiriak sistemak ez du oraindik hasieratu egin dira. Jarri harremanetan sistemako administratzailearekin informazio gehiago eskuratzeko.';
+$lang['user_certificates_configuration_file'] = 'Konfigurazio fitxategia';
+$lang['user_certificates_create_certificates'] = 'Sortu ziurtagiriak';
+$lang['user_certificates_initialize_certificate_help'] = 'Mesedez, zehaztu pasahitz bat zure segurtasun ziurtagiriak babesteko. ahaztu duzu pasahitza, baino gehiago hasteko ziurtagiriak multzo berri bat behar izango duzu.';
