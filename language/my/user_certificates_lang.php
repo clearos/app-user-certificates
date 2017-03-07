87c99300c90a96bb0d7f53a775046862de0c29d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:43:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'လုံခြုံရေးလက်မှတ်များကိုသငျသညျတစျနေ့နေ့အခြေခံပေါ်မှာအသုံးပြုအမျိုးမျိုးသော apps များလုံခြုံရန်အသုံးပြုကြသည်။';
+$lang['user_certificates_app_name'] = 'အသုံးပြုသူလက်မှတ်';
+$lang['user_certificates_certificates'] = 'လက်မှတ်များ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'အသုံးပြုသူလက်မှတ်များဒီအကောင့်အတွက်ရရှိနိုင်မရှိကြပေ။';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ဝမ်းနည်းပါတယ်, security certificates ကိုဒီစနစ်အပေါ်မရရှိနိုင်။ နောက်ထပ်သတင်းအချက်အလက်များအဘို့အသင့် system အုပ်ချုပ်ရေးမှူးကိုဆက်သွယ်ပါ။';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ဝမ်းနည်းပါတယ်, security certificates ကိုသင့်အကောင့်များအတွက် enabled ကြသည်မဟုတ်။';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ဝမ်းနည်းပါတယ်, လုံခြုံရေးလက်မှတ်များစနစ်အသေးစတင်ဖို့မခံတော်မူပြီ။ နောက်ထပ်သတင်းအချက်အလက်များအဘို့အသင့် system အုပ်ချုပ်ရေးမှူးကိုဆက်သွယ်ပါ။';
+$lang['user_certificates_configuration_file'] = 'configuration ဖိုင်';
+$lang['user_certificates_create_certificates'] = 'လက်မှတ် Create';
+$lang['user_certificates_initialize_certificate_help'] = 'သင့်ရဲ့ security certificates ကိုကာကွယ်စောင့်ရှောက်ဘို့ password တစ်ခုသတ်မှတ်ပေးပါ။ သင်စကားဝှက်ကိုမေ့လျှင်သင်တို့ကိုလက်မှတ်များ၏အသစ်တစ်စုံနှင့်အတူပြန်စဖို့ရန်လိုအပ်ပါလိမ့်မည်။';
