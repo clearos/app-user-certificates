9ff0ae62a4c89f9d1609ac9dc555162e695746e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:46:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'સુરક્ષા પ્રમાણપત્રો છે જે તમે દિવસ ધોરણે એક દિવસ પર ઉપયોગ વિવિધ એપ્લિકેશન્સ સુરક્ષિત કરવા માટે ઉપયોગ થાય છે.';
+$lang['user_certificates_app_name'] = 'વપરાશકર્તા પ્રમાણપત્રો';
+$lang['user_certificates_certificates'] = 'પ્રમાણપત્રો';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'વપરાશકર્તા પ્રમાણપત્ર આ એકાઉન્ટ માટે ઉપલબ્ધ નથી.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'માફ કરશો, સુરક્ષા પ્રમાણપત્રો આ સિસ્ટમ પર ઉપલબ્ધ નથી. વધુ માહિતી માટે તમારા સિસ્ટમ સંચાલકનો સંપર્ક કરો.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'માફ કરશો, સુરક્ષા પ્રમાણપત્રો તમારા એકાઉન્ટ માટે સક્ષમ નથી.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'માફ કરશો, સુરક્ષા પ્રમાણપત્રો સિસ્ટમ હજુ સુધી નથી આરંભ કરવામાં આવ્યો છે. વધુ માહિતી માટે તમારા સિસ્ટમ સંચાલકનો સંપર્ક કરો.';
+$lang['user_certificates_configuration_file'] = 'રૂપરેખાંકન ફાઈલ';
+$lang['user_certificates_create_certificates'] = 'પ્રમાણપત્રો બનાવો';
+$lang['user_certificates_initialize_certificate_help'] = 'તમારી સુરક્ષા પ્રમાણપત્રો રક્ષણ માટે પાસવર્ડ સ્પષ્ટ કરો. જો તમે પાસવર્ડ ભૂલી ગયા હો, તો તમે પ્રમાણપત્રો એક નવો સેટ સાથે શરૂ કરવાની જરૂર પડશે.';
