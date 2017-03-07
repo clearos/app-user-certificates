de1fa024caf25ff16243f4e1ac7cdb1938ed652a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'סעקוריטי certificates זענען געניצט צו באַוואָרענען פאַרשידן אַפּפּס אַז איר נוצן אויף אַ טאָג צו טאָג יקער.';
+$lang['user_certificates_app_name'] = 'באַניצער certificates';
+$lang['user_certificates_certificates'] = 'certificates';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'באַניצער certificates זענען נישט בנימצא פֿאַר דעם חשבון.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'אנטשולדיגט, זיכערהייַט certificates זענען נישט בנימצא אויף דעם סיסטעם. ביטע קאָנטאַקט דיין סיסטעם אַדמיניסטראַטאָר פֿאַר מער אינפֿאָרמאַציע.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'אנטשולדיגט, זיכערהייַט certificates זענען נישט ענייבאַלד פֿאַר דיין חשבון.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'אנטשולדיגט, די זיכערהייַט certificates סיסטעם האט נישט נאָך געווען יניטיאַליזעד. ביטע קאָנטאַקט דיין סיסטעם אַדמיניסטראַטאָר פֿאַר מער אינפֿאָרמאַציע.';
+$lang['user_certificates_configuration_file'] = 'קאָנפיגוראַטיאָן טעקע';
+$lang['user_certificates_create_certificates'] = 'שאַפֿן certificates';
+$lang['user_certificates_initialize_certificate_help'] = 'ביטע ספּעציפיצירן אַ פּאַראָל פֿאַר פּראַטעקטינג אייער זיכערהייַט certificates. אויב איר פאַרגעסן די פּאַראָל, איר וועט דאַרפֿן צו אָנהייבן איבער מיט אַ נייַ שטעלן פון certificates.';
