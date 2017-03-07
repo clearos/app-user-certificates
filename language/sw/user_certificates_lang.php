4e9a6126e357d98445e195170f3254c33b941523
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'vyeti vya usalama ni kutumika kupata programu mbalimbali kwamba matumizi ya siku hadi siku.';
+$lang['user_certificates_app_name'] = 'Vyeti mtumiaji';
+$lang['user_certificates_certificates'] = 'vyeti';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Vyeti vya mtumiaji hazipatikani kwa akaunti hii.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Sorry, vyeti vya usalama ni si inapatikana kwenye mfumo huu. Tafadhali wasiliana na mtawala wako kwa maelezo zaidi.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Sorry, vyeti vya usalama si kuwezeshwa kwa akaunti yako.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Samahani, mfumo vyeti vya usalama bado initialized. Tafadhali wasiliana na mtawala wako kwa maelezo zaidi.';
+$lang['user_certificates_configuration_file'] = 'Configuration faili';
+$lang['user_certificates_create_certificates'] = 'kujenga Vyeti';
+$lang['user_certificates_initialize_certificate_help'] = 'Tafadhali kutaja password kwa ajili ya kulinda vyeti usalama wako. Kama kusahau password, unahitaji kuanza juu na kuweka mpya ya vyeti.';
