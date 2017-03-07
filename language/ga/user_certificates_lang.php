0dae5325e4989f6344927c1cfcfa3b885faf82d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'deimhnithe slándála a úsáidtear chun apps éagsúla a úsáideann tú ar bhonn laethúil a bhaint amach.';
+$lang['user_certificates_app_name'] = 'Teastais Úsáideora';
+$lang['user_certificates_certificates'] = 'Deimhnithe';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Níl teastais Úsáideoir fáil don chuntas seo.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Is oth linn nach bhfuil deimhnithe slándála atá ar fáil ar an gcóras seo. Téigh i dteagmháil le riarthóir do chórais le tuilleadh eolais.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Tá brón orainn, nach bhfuil deimhnithe slándála cumasaithe do do chuntas.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Tá brón orainn, nach bhfuil an córas deimhnithe slándála túsaithe fóill. Téigh i dteagmháil le riarthóir do chórais le tuilleadh eolais.';
+$lang['user_certificates_configuration_file'] = 'Comhad cumraíochta';
+$lang['user_certificates_create_certificates'] = 'Cruthaigh Deimhnithe';
+$lang['user_certificates_initialize_certificate_help'] = 'Sonraigh focal faire a chosaint do dheimhnithe slándála. Má tá tú dearmad an focal faire, beidh ort chun tús a chur thar le sraith nua de theastais.';
