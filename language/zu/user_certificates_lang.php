3c823645237af15367cb2648de2c595909fc85c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'izitifiketi Security zisetshenziselwa ukuze aqinisekise zokusebenza ahlukahlukene ukuthi usebenzisa usuku nosuku.';
+$lang['user_certificates_app_name'] = 'Umsebenzisi Izitifiketi';
+$lang['user_certificates_certificates'] = 'Izitifiketi';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'izitifiketi Umsebenzisi azitholakali kule akhawunti.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Uxolo, izitifiketi zokuphepha azitholakali lesi simiso. Sicela uxhumane nomlawuli wesistimu yakho ukuze uthole ulwazi oluthe xaxa.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Uxolo, izitifiketi zokuphepha kungukuthi amandla ku-akhawunti yakho.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Uxolo, uhlelo izitifiketi zokuphepha ingakamiswa ukuqaliswa. Sicela uxhumane nomlawuli wesistimu yakho ukuze uthole ulwazi oluthe xaxa.';
+$lang['user_certificates_configuration_file'] = 'Ukucushwa Ifayela';
+$lang['user_certificates_create_certificates'] = 'Dala Izitifiketi';
+$lang['user_certificates_initialize_certificate_help'] = 'Sicela ucacise iphasiwedi ekuvikeleni izitifiketi zakho zokuvikeleka. Uma ukhohlwa iphasiwedi, uzodinga ukuqala abasha izitifiketi.';
