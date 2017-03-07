074e8ad9491ad2390152bbdb21271a22a17f1f21
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'шаҳодатномаҳои амниятӣ истифода бурда мешаванд, ба таъмини Барномаҳои гуногун, ки ба шумо дар як рӯз ба рӯз дар асоси истифода баред.';
+$lang['user_certificates_app_name'] = 'Сертификати корбар';
+$lang['user_certificates_certificates'] = 'Шаҳодатномаҳо';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'сертификатҳои корбар дастрас нестанд, ба ин ҳисоби.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Бубахшед, шаҳодатномаҳои амниятӣ мавҷуд дар ин система нест. Лутфан, бо администратори системаи шуморо Барои иттилооти бештар дар тамос шавед.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Бубахшед, шаҳодатномаҳои амниятӣ барои ҳисоби шумо ба кор андохта нашудааст.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Мутаасифона, системаи шаҳодатномаҳои амниятӣ ҳанӯз initialized нашудааст. Лутфан, бо администратори системаи шуморо Барои иттилооти бештар дар тамос шавед.';
+$lang['user_certificates_configuration_file'] = 'Батанзимдарорӣ File';
+$lang['user_certificates_create_certificates'] = 'Эҷоди Шаҳодатномаҳо';
+$lang['user_certificates_initialize_certificate_help'] = 'Марҳамат карда барои ҳифзи амнияти худ шаҳодатномаҳои муайян мекунад. Агар Нишонае, ки шумо фаромӯш, шумо лозим меояд, ки ба беш аз оғоз бо маҷмӯи нави сертификатҳои.';
