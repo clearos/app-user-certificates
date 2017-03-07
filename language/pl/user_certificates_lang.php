a5858379257c5b2cf8aa236ee4dbc4d30f32ed02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:55:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'certyfikaty bezpieczeństwa służą do zabezpieczenia różnych aplikacji, których używasz na co dzień.';
+$lang['user_certificates_app_name'] = 'Certyfikaty użytkownika';
+$lang['user_certificates_certificates'] = 'certyfikaty';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Certyfikaty użytkownika nie są dostępne dla tego konta.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Niestety, certyfikaty bezpieczeństwa nie są dostępne w tym systemie. Proszę skontaktować się z administratorem systemu, aby uzyskać więcej informacji.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Niestety, certyfikaty bezpieczeństwa nie są włączone dla konta.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Niestety, system certyfikaty bezpieczeństwa nie został jeszcze zainicjowany. Proszę skontaktować się z administratorem systemu, aby uzyskać więcej informacji.';
+$lang['user_certificates_configuration_file'] = 'Plik konfiguracyjny';
+$lang['user_certificates_create_certificates'] = 'Tworzenie certyfikatów';
+$lang['user_certificates_initialize_certificate_help'] = 'Podaj hasło do ochrony certyfikaty bezpieczeństwa. Jeśli użytkownik zapomni hasła, trzeba będzie zacząć od nowa z nowym zestawem certyfikatów.';
