d1f31d45cbda78ba2e94f9836c64ee7435b89a5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Güvenlik sertifikalar günlük olarak bir gün kullanmak çeşitli uygulamaları sabitlemek için kullanılır.';
+$lang['user_certificates_app_name'] = 'kullanıcı Belgeleri';
+$lang['user_certificates_certificates'] = 'Sertifikalar';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Kullanıcı sertifikaları Bu hesap için geçerli değildir.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Maalesef güvenlik sertifikaları, bu sistemde mevcut değildir. Daha fazla bilgi için sistem yöneticisine başvurun.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Maalesef güvenlik sertifikaları hesabınız için etkin değil.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Maalesef güvenlik sertifikaları sistemi henüz başlatılmamış. Daha fazla bilgi için sistem yöneticisine başvurun.';
+$lang['user_certificates_configuration_file'] = 'yapılandırma Dosyası';
+$lang['user_certificates_create_certificates'] = 'Sertifikalar oluşturun';
+$lang['user_certificates_initialize_certificate_help'] = 'Güvenlik sertifikaları korumak için bir parola belirtin. Eğer şifrenizi unutursanız, sertifika yeni bir dizi ile başlamak gerekir.';
