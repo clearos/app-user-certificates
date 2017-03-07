85d67fd2cc505bc548b4aa025a7b4e90262fb973
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Certificatele de securitate sunt utilizate pentru a asigura diverse aplicații pe care le utilizați într-o zi de zi.';
+$lang['user_certificates_app_name'] = 'Certificatele de utilizator';
+$lang['user_certificates_certificates'] = 'certificate';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Certificatele de utilizator nu sunt disponibile pentru acest cont.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Ne pare rău, certificate de securitate nu sunt disponibile pe acest sistem. Vă rugăm să contactați administratorul de sistem pentru mai multe informații.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Ne pare rău, certificatele de securitate nu sunt activate pentru contul tău.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Ne pare rău, sistemul de certificate de securitate nu a fost încă inițializat. Vă rugăm să contactați administratorul de sistem pentru mai multe informații.';
+$lang['user_certificates_configuration_file'] = 'Fișier de configurare';
+$lang['user_certificates_create_certificates'] = 'Creați Certificate';
+$lang['user_certificates_initialize_certificate_help'] = 'Vă rugăm să specificați o parolă pentru a proteja certificatele de securitate. Dacă ați uitat parola, va trebui să înceapă peste cu un nou set de certificate.';
