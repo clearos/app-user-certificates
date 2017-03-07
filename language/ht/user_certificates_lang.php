e358719dae08942e48e448383d131d9a5f9de0a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:46:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'sètifika Sekirite yo te itilize sa yo an sekirite apps divès kalite ke ou sèvi ak sou yon jou nan baz jou.';
+$lang['user_certificates_app_name'] = 'itilizatè Sètifika';
+$lang['user_certificates_certificates'] = 'sètifika';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Itilizatè sètifika yo pa disponib pou kont sa a.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Padon, sètifika sekirite yo pa disponib sou sistèm sa a. Tanpri kontakte administratè sistèm ou pou plis enfòmasyon.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Padon, sètifika sekirite pa yo ap pèmèt pou kont ou.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Padon, sistèm nan sètifika sekirite pa gen ankò te inisyalizèd. Tanpri kontakte administratè sistèm ou pou plis enfòmasyon.';
+$lang['user_certificates_configuration_file'] = 'nou konte ofri sèvis File';
+$lang['user_certificates_create_certificates'] = 'Kreye Sètifika';
+$lang['user_certificates_initialize_certificate_help'] = 'Tanpri endike yon modpas pou pwoteje sètifika sekirite ou. Si ou ta bliye modpas la, ou pral bezwen kòmanse sou ak yon seri nouvo sou sètifika.';
