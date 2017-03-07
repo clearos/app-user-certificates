a6def93728a58c3d210c065dec867d6ee88aae0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:45:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Turvalisus sertifikaate kasutatakse kindlustada erinevate rakenduste, mida saate kasutada igapäevase alusel.';
+$lang['user_certificates_app_name'] = 'Kasutaja sertifikaadid';
+$lang['user_certificates_certificates'] = 'Sertifikaadid';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Kasutaja sertifikaadid ei ole selle konto.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Vabandame, turvalisuse sertifikaate ei ole selles süsteemis. Palun pöörduge oma süsteemiadministraatori poole rohkem teavet.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Vabandame, turvalisuse sertifikaate ei võimaldanud oma konto.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Vabandame, turvalisuse sertifikaadid süsteem ei ole veel vormindatud. Palun pöörduge oma süsteemiadministraatori poole rohkem teavet.';
+$lang['user_certificates_configuration_file'] = 'Seadistusfaili';
+$lang['user_certificates_create_certificates'] = 'Loo Sertifikaadid';
+$lang['user_certificates_initialize_certificate_help'] = 'Palun sisesta parool, et kaitsta oma turvalisuse sertifikaate. Kui unustate parooli, peate otsast peale alustama uusi sertifikaate.';
