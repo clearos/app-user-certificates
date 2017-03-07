191affb32dd3be3be2f875a69d37b8339abfae1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Öryggi vottorð eru notuð til að tryggja ýmis forrit sem þú notar á degi til dags.';
+$lang['user_certificates_app_name'] = 'User certificates';
+$lang['user_certificates_certificates'] = 'vottorð';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Notandavottorð eru ekki í boði fyrir þennan reikning.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Því miður, eru öryggis vottorð ekki í boði á þessu kerfi. Vinsamlegast hafðu samband við kerfisstjóra til að fá frekari upplýsingar.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Því miður, eru öryggis vottorð ekki virk fyrir reikninginn þinn.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Því miður, þá öryggi vottorð kerfið hefur ekki enn verið forsniðin. Vinsamlegast hafðu samband við kerfisstjóra til að fá frekari upplýsingar.';
+$lang['user_certificates_configuration_file'] = 'stillingaskrá';
+$lang['user_certificates_create_certificates'] = 'Búa Vottorð';
+$lang['user_certificates_initialize_certificate_help'] = 'Vinsamlegast tilgreindu lykilorð til að vernda öryggisvottorð. Ef þú gleymir lykilorðinu, þú þarft að byrja aftur með nýtt sett af vottorðum.';
