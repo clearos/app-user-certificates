8e3fe4639cb99650539042e2339ffb137cd7aa14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:44:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Els certificats de seguretat es fan servir per assegurar diverses aplicacions que s&#39;utilitzen en el dia a dia.';
+$lang['user_certificates_app_name'] = 'Els certificats d&#39;usuari';
+$lang['user_certificates_certificates'] = 'certificats';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Els certificats d&#39;usuari no estan disponibles per a aquest compte.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Ho sentim, certificats de seguretat no estan disponibles en aquest sistema. Si us plau, poseu-vos en contacte amb l&#39;administrador del sistema per obtenir més informació.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Ho sentim, certificats de seguretat no estan habilitades per a la seva compte.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Ho sentim, el sistema de certificats de seguretat encara no s&#39;ha inicialitzat. Si us plau, poseu-vos en contacte amb l&#39;administrador del sistema per obtenir més informació.';
+$lang['user_certificates_configuration_file'] = 'fitxer de configuració';
+$lang['user_certificates_create_certificates'] = 'crear Certificats';
+$lang['user_certificates_initialize_certificate_help'] = 'Si us plau especificar una contrasenya per protegir els seus certificats de seguretat. Si oblideu la contrasenya, haurà de començar de nou amb un nou conjunt de certificats.';
