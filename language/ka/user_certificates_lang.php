894b7d8915551187a3e4dfcd00ba092e8259214c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'უსაფრთხოების სერთიფიკატები გამოიყენება უზრუნველყოფა სხვადასხვა პროგრამები, რომელიც გამოიყენებს დღეს დღეს საფუძველზე.';
+$lang['user_certificates_app_name'] = 'შესახებ სერთიფიკატები';
+$lang['user_certificates_certificates'] = 'სერთიფიკატები';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'მომხმარებლის სერთიფიკატები არ არის ხელმისაწვდომი ამ ანგარიშზე.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'სამწუხაროდ, უსაფრთხოების სერთიფიკატები არ არის შესაძლებელი ამ სისტემაში. გთხოვთ დაუკავშირდეთ სისტემის ადმინისტრატორს დამატებითი ინფორმაცია.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'სამწუხაროდ, უსაფრთხოების სერთიფიკატები არ არის გააქტიურებული თქვენი ანგარიში.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'უკაცრავად, უსაფრთხოების სერთიფიკატები სისტემა ჯერ კიდევ არ არის ინიცირებული. გთხოვთ დაუკავშირდეთ სისტემის ადმინისტრატორს დამატებითი ინფორმაცია.';
+$lang['user_certificates_configuration_file'] = 'კონფიგურაციის ფაილი';
+$lang['user_certificates_create_certificates'] = 'შექმნა სერთიფიკატები';
+$lang['user_certificates_initialize_certificate_help'] = 'გთხოვთ, მიუთითოთ პაროლი დასაცავად თქვენი უსაფრთხოების სერთიფიკატები. თუ თქვენ დაგავიწყდათ პაროლი, თქვენ უნდა დაიწყოს დასრულდა ახალი კომპლექტი სერთიფიკატები.';
