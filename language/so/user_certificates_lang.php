1a7de2d88fb4d76f6d6573e78be11f48f607a2be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'shahaadooyinka Security waxaa loo isticmaalaa si loo sugo apps kala duwan oo aad u isticmaali maalin si ku salaysan maalin.';
+$lang['user_certificates_app_name'] = 'Certificates User';
+$lang['user_certificates_certificates'] = 'Certificates';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'shahaadooyinka User ma ayaa diyaar u ah account this.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Sorry, shahaadooyinka ammaanka ma waxaa laga heli karaa nidaamka this. Fadlan la xiriir maamulaha aad u hesho macluumaad dheeraad ah.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Sorry, shahaadooyinka ammaanka aan karti aad account.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Sorry, nidaamka shahaadada ammaanka oo aan weli la initialized. Fadlan la xiriir maamulaha aad u hesho macluumaad dheeraad ah.';
+$lang['user_certificates_configuration_file'] = 'qaabeynta File';
+$lang['user_certificates_create_certificates'] = 'Abuur Certificates';
+$lang['user_certificates_initialize_certificate_help'] = 'Fadlan sheeg password a ilaalinta shahaadooyin ammaanka. Haddii aad illowdo password ka, waxaad u baahan doontaa inuu ku soo bilowdo ka badan la set cusub oo shahaadooyin.';
