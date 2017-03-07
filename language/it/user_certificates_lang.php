f016d288b00f4a98138130300267ffa2bf506e17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'I certificati di protezione vengono utilizzati per fissare varie applicazioni che si usano in un giorno per giorno.';
+$lang['user_certificates_app_name'] = 'certificati utente';
+$lang['user_certificates_certificates'] = 'certificati';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'I certificati utente non sono disponibili per questo account.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Siamo spiacenti, certificati di sicurezza non sono disponibili su questo sistema. Si prega di contattare l&#39;amministratore di sistema per ulteriori informazioni.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Siamo spiacenti, certificati di protezione non sono abilitati per il tuo account.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Spiacenti, il sistema di certificati di sicurezza non è ancora stato inizializzato. Si prega di contattare l&#39;amministratore di sistema per ulteriori informazioni.';
+$lang['user_certificates_configuration_file'] = 'File di configurazione';
+$lang['user_certificates_create_certificates'] = 'creare certificati';
+$lang['user_certificates_initialize_certificate_help'] = 'Si prega di specificare una password per proteggere i certificati di sicurezza. Se si dimentica la password, è necessario ricominciare da capo con un nuovo set di certificati.';
