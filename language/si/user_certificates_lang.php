7f7dc9b815d4134dad9e076241d15241b702b591
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ආරක්ෂක සහතික ඔබ එදිනෙදා කිරීමට පදනම මත භාවිතා කරන විවිධ යෙදුම් සුරක්ෂිත කිරීම සඳහා භාවිතා වේ.';
+$lang['user_certificates_app_name'] = 'පරිශීලක සහතික';
+$lang['user_certificates_certificates'] = 'සහතික';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'පරිශීලක සහතික මෙම ගිණුම සඳහා ලබා ගත හැකි නොවේ.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'සමාවන්න, ආරක්ෂක සහතික මෙම පද්ධතිය නොවේ ලබා ගත හැකිය. වැඩි විස්තර සඳහා කරුණාකර ඔබගේ පද්ධති පරිපාලක හමුවන්න.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'සමාවන්න, ඔබගේ ගිණුම සඳහා ආරක්ෂක සහතික සක්රීය කොට නැත.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'සමාවන්න, ආරක්ෂක සහතික පද්ධතිය තවමත් ආරම්භනය කර නැත. වැඩි විස්තර සඳහා කරුණාකර ඔබගේ පද්ධති පරිපාලක හමුවන්න.';
+$lang['user_certificates_configuration_file'] = 'මානකරන ගොනුව';
+$lang['user_certificates_create_certificates'] = 'සහතික නිර්මාණය';
+$lang['user_certificates_initialize_certificate_help'] = 'ඔබගේ ආරක්ෂාව සහතික ආරක්ෂා කිරීම සඳහා රහස්පදයක් කරන්න. ඔබ රහස් වචනය අමතක කරයි නම්, ඔබ සහතික නව කට්ටලයක් සමග නැවත ආරම්භ කිරීමට අවශ්ය වනු ඇත.';
