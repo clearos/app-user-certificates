d0a59ff05470d6e0351ddd7f514b3d55f4cfe66a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Безбедносните сертификати се користат за да се обезбеди различни апликации кои ги користите на ден на ден основа.';
+$lang['user_certificates_app_name'] = 'сертификати за корисникот';
+$lang['user_certificates_certificates'] = 'сертификати';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Кориснички сертификати не се достапни за оваа сметка.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Жал ми е, безбедносни сертификати не се достапни на овој систем. Ве молиме контактирајте со вашиот систем администратор за повеќе информации.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Жал ми е, безбедносни сертификати не се дозволени за вашиот профил.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Жал ми е, системот на безбедносни сертификати се уште не е иницијализиран. Ве молиме контактирајте со вашиот систем администратор за повеќе информации.';
+$lang['user_certificates_configuration_file'] = 'конфигурациската датотека';
+$lang['user_certificates_create_certificates'] = 'Креирај сертификати';
+$lang['user_certificates_initialize_certificate_help'] = 'Ве молиме наведете лозинката за заштита на вашите безбедносни сертификати. Ако ја заборавите лозинката, ќе треба да се започне одново со нов сет на сертификати.';
