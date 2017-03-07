d240dbd6c11546b84242d1dae3c0adab2117e6f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = '보안 인증서는 하루 기준으로 하루에 사용하는 다양한 응용 프로그램을 보호하는 데 사용됩니다.';
+$lang['user_certificates_app_name'] = '사용자 인증서';
+$lang['user_certificates_certificates'] = '인증서';
+$lang['user_certificates_certificates_not_available_for_this_account'] = '사용자 인증서는이 계정을 사용할 수 없습니다.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = '죄송합니다, 보안 인증서는이 시스템에서 사용할 수 없습니다. 자세한 내용은 시스템 관리자에게 문의하십시오.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = '죄송합니다, 보안 인증서는 귀하의 계정에 사용할 수 없습니다.';
+$lang['user_certificates_certificates_not_yet_initialized'] = '죄송합니다, 보안 인증 시스템은 아직 초기화되지 않았습니다. 자세한 내용은 시스템 관리자에게 문의하십시오.';
+$lang['user_certificates_configuration_file'] = '구성 파일';
+$lang['user_certificates_create_certificates'] = '인증서 만들기';
+$lang['user_certificates_initialize_certificate_help'] = '보안 인증서를 보호하기위한 암호를 지정하십시오. 암호를 잊어 버린 경우 인증서의 새로운 세트로 시작해야합니다.';
