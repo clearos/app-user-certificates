97d0b1a056836c3475b4f6a8c91aba27c1ce2c8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:45:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Turvallisuus varmenteita käytetään suojaamaan erilaisia ​​sovelluksia, että käytät on päivittäin.';
+$lang['user_certificates_app_name'] = 'Käyttäjä Sertifikaatit';
+$lang['user_certificates_certificates'] = 'Sertifikaatit';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Käyttäjä varmenteet eivät ole käytettävissä tälle tilille.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Valitettavasti varmenteita ei ole käytettävissä tässä järjestelmässä. Ota yhteyttä järjestelmän ylläpitäjään.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Valitettavasti varmenteita ei ole otettu käyttöön tililläsi.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Valitettavasti varmenteita järjestelmä ei ole vielä alustettu. Ota yhteyttä järjestelmän ylläpitäjään.';
+$lang['user_certificates_configuration_file'] = 'määritystiedosto';
+$lang['user_certificates_create_certificates'] = 'Luo Sertifikaatit';
+$lang['user_certificates_initialize_certificate_help'] = 'Määritä salasana suojaamaan varmenteita. Jos unohdat salasanan, sinun täytyy aloittaa alusta uudet todistukset.';
