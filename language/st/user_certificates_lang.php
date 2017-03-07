0645823dc5d74bf4b328516814c0ff7cb7b94ee4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:56:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'disetifikeiti Security li sebelisetsoa ho boloka ditiriso tse fapa-fapaneng hore u sebelise ka letsatsi le letsatsi motheo oa.';
+$lang['user_certificates_app_name'] = 'disetifikeiti User';
+$lang['user_certificates_certificates'] = 'disetifikeiti';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'disetifikeiti User ke ha e fumanehe bakeng sa tlaleho ena.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Tšoarelo, disetifikeiti tshireletso ke ha e fumanehe tsamaisong ena. Kotjwa hore o iteanye le motsamaisi hao tsamaiso bakeng sa tlhahisoleseding e eketsehileng.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Tšoarelo, disetifikeiti tshireletso ha ba nolofalletsa bakeng akhaonteng ya hao.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Tšoarelo, tsamaiso disetifikeiti tshireletso e-s&#39;o initialized. Kotjwa hore o iteanye le motsamaisi hao tsamaiso bakeng sa tlhahisoleseding e eketsehileng.';
+$lang['user_certificates_configuration_file'] = 'phetolo File';
+$lang['user_certificates_create_certificates'] = 'Create mangolo';
+$lang['user_certificates_initialize_certificate_help'] = 'Ka kōpo bolela ka ho toba e le phasewete bakeng sa ho sireletsa disetifikeiti ea hao tshireletso. Haeba u lebala phasewete, u tla lokela ho qalella ho lilemo tse fetang le sete e ncha ea disetifikeiti.';
