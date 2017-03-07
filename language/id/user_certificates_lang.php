f70de14c756efa3d16cce867d50d27b252648b46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'sertifikat keamanan yang digunakan untuk mengamankan berbagai aplikasi yang Anda gunakan pada hari ke hari.';
+$lang['user_certificates_app_name'] = 'Sertifikat pengguna';
+$lang['user_certificates_certificates'] = 'sertifikat';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Sertifikat pengguna tidak tersedia untuk akun ini.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Maaf, sertifikat keamanan yang tidak tersedia pada sistem ini. Silahkan hubungi administrator sistem Anda untuk informasi lebih lanjut.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Maaf, sertifikat keamanan tidak diaktifkan untuk akun Anda.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Maaf, sistem sertifikat keamanan belum diinisialisasi. Silahkan hubungi administrator sistem Anda untuk informasi lebih lanjut.';
+$lang['user_certificates_configuration_file'] = 'file konfigurasi';
+$lang['user_certificates_create_certificates'] = 'Buat Sertifikat';
+$lang['user_certificates_initialize_certificate_help'] = 'Silakan tentukan password untuk melindungi sertifikat keamanan Anda. Jika Anda lupa password, Anda akan perlu untuk memulai dengan satu set baru sertifikat.';
