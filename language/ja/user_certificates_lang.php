05bd8387069b583bda5ddef6cbd298550559effc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:47:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'セキュリティ証明書を使用すると、日常的に使用するさまざまなアプリケーションを保護するために使用されています。';
+$lang['user_certificates_app_name'] = 'ユーザー証明書';
+$lang['user_certificates_certificates'] = '証明書';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ユーザー証明書は、このアカウントでは使用できません。';
+$lang['user_certificates_certificates_not_available_on_this_system'] = '申し訳ありませんが、セキュリティ証明書は、このシステムでは使用できません。 詳細については、システム管理者に連絡してください。';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = '申し訳ありませんが、セキュリティ証明書は、アカウントが有効になっていません。';
+$lang['user_certificates_certificates_not_yet_initialized'] = '申し訳ありませんが、セキュリティ証明書システムがまだ初期化されていません。 詳細については、システム管理者に連絡してください。';
+$lang['user_certificates_configuration_file'] = '設定ファイル';
+$lang['user_certificates_create_certificates'] = '証明書を作成します。';
+$lang['user_certificates_initialize_certificate_help'] = 'あなたのセキュリティ証明書を保護するためのパスワードを指定してください。 パスワードを忘れた場合は、証明書の新しいセットで最初からやり直す必要があります。';
