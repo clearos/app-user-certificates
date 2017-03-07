474952ed23a55ab13c52225b199e8783eaae6f96
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Beveiligingscertificaten worden gebruikt om verschillende apps die u gebruikt op een dagelijkse basis te beveiligen.';
+$lang['user_certificates_app_name'] = 'gebruiker Certificaten';
+$lang['user_certificates_certificates'] = 'certificaten';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Gebruikerscertificaten zijn niet beschikbaar voor dit account.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Sorry, beveiligingscertificaten niet beschikbaar zijn op dit systeem. Neem contact op met uw systeembeheerder voor meer informatie.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Sorry, beveiligingscertificaten zijn niet ingeschakeld voor uw account.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Sorry, de beveiligingscertificaten systeem is nog niet geïnitialiseerd. Neem contact op met uw systeembeheerder voor meer informatie.';
+$lang['user_certificates_configuration_file'] = 'Configuratiebestand';
+$lang['user_certificates_create_certificates'] = 'Maak Certificaten';
+$lang['user_certificates_initialize_certificate_help'] = 'Geef een wachtwoord voor het beschermen van uw beveiligingscertificaten. Als u het wachtwoord vergeet, moet u beginnen met een nieuwe reeks van certificaten.';
