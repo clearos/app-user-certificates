477a5927d611070bedf772ad097e34fbe5028cb9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Аюулгүй байдлын гэрчилгээ та өдөр тутам нэг өдөр хэрэглэх нь төрөл бүрийн програм аюулгүй болгоход хэрэглэгдэх юм.';
+$lang['user_certificates_app_name'] = 'Хэрэглэгчийн гэрчилгээ';
+$lang['user_certificates_certificates'] = 'гэрчилгээ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Хэрэглэгчийн гэрчилгээ энэ дансанд байхгүй байна.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Уучлаарай, аюулгүй байдлын гэрчилгээ энэ систем дээр байхгүй байна. Дэлгэрэнгүй мэдээллийг таны системийн админтай холбогдоно уу.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Уучлаарай, аюулгүй байдлын гэрчилгээ таны дансанд идэвхжүүлсэн байна.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Уучлаарай, аюулгүй байдлын гэрчилгээ систем нь ч эхэлж байсан байна. Дэлгэрэнгүй мэдээллийг таны системийн админтай холбогдоно уу.';
+$lang['user_certificates_configuration_file'] = 'тохиргооны файл';
+$lang['user_certificates_create_certificates'] = 'Сертификатуудыг үүсгэх';
+$lang['user_certificates_initialize_certificate_help'] = 'Таны аюулгүй байдлын гэрчилгээ хамгаалах нууц үгээ оруулна уу. Та нууц үгээ мартсан бол та гэрчилгээний шинэ багц нь эхлэх хэрэгтэй болно.';
