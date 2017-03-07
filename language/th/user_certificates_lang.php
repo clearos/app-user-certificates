a1aa5c36df9e78608bf4970f77c8c67042d611fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ใบรับรองความปลอดภัยจะใช้ในการรักษาความปลอดภัยปพลิเคชันต่างๆที่คุณใช้ในวันที่ตามวัน';
+$lang['user_certificates_app_name'] = 'ใบรับรองผู้ใช้';
+$lang['user_certificates_certificates'] = 'ใบรับรอง';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ใบรับรองผู้ใช้จะไม่สามารถใช้ได้สำหรับบัญชีนี้';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ขออภัยใบรับรองความปลอดภัยจะไม่สามารถใช้ได้ในระบบนี้ กรุณาติดต่อผู้ดูแลระบบของคุณสำหรับข้อมูลเพิ่มเติม';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ขออภัยใบรับรองความปลอดภัยยังไม่ได้เปิดใช้งานสำหรับบัญชีของคุณ';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ขออภัยระบบใบรับรองความปลอดภัยยังไม่ได้เริ่มต้น กรุณาติดต่อผู้ดูแลระบบของคุณสำหรับข้อมูลเพิ่มเติม';
+$lang['user_certificates_configuration_file'] = 'แฟ้มการกำหนดค่า';
+$lang['user_certificates_create_certificates'] = 'สร้างใบรับรอง';
+$lang['user_certificates_initialize_certificate_help'] = 'กรุณาระบุรหัสผ่านสำหรับการปกป้องใบรับรองความปลอดภัยของคุณ หากคุณลืมรหัสผ่านที่คุณจะต้องเริ่มต้นใหม่กับชุดใหม่ของใบรับรอง';
