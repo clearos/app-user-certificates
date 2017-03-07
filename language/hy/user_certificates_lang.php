bb79036559c211fb768265df36eada7bfa5c4c4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:41:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Անվտանգության վկայականներ օգտագործվում են ապահովելու տարբեր ծրագրեր, որոնք դուք կարող եք օգտագործել մի օր հիման վրա.';
+$lang['user_certificates_app_name'] = 'User Վկայականներ';
+$lang['user_certificates_certificates'] = 'Հավաստագրեր';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Օգտվող հավաստագրերը չեն հասանելի են այս հաշվում.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Ներեցեք, անվտանգության հավաստագրերը չեն հասանելի այս համակարգում: Խնդրում ենք կապնվել ձեր համակարգի ադմինիստրատորին Լրացուցիչ տեղեկությունների համար.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Ներեցեք, անվտանգության հավաստագրերը չեն միացված ձեր հաշվի համար:';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Ներեցեք, անվտանգության վկայագրեր համակարգը դեռեւս չի սկզբնավորված: Խնդրում ենք կապնվել ձեր համակարգի ադմինիստրատորին Լրացուցիչ տեղեկությունների համար.';
+$lang['user_certificates_configuration_file'] = 'կազմաձեւման ֆայլը';
+$lang['user_certificates_create_certificates'] = 'Ստեղծել վկայականները';
+$lang['user_certificates_initialize_certificate_help'] = 'Խնդրում ենք նշեք գաղտնաբառը պաշտպանելու ձեր անվտանգության վկայականներ: Եթե ​​մոռանաք գաղտնաբառը, դուք պետք է սկսել մի նոր շարք հավաստագրեր:';
