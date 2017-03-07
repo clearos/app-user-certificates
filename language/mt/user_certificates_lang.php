2fe20a8ae93edb66838ab74fb8760c1714b7c745
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ċertifikati ta &#39;sigurtà jintużaw sabiex jiġi akkwistat apps varji li inti użu fuq bażi ta&#39; kuljum.';
+$lang['user_certificates_app_name'] = 'Ċertifikati utent';
+$lang['user_certificates_certificates'] = 'ċertifikati';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'utent aħħari ma jkunux disponibbli għal dan il-kont.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Jiddispjacini, ċertifikati tas-sigurtà mhumiex disponibbli fuq din is-sistema. Jekk jogħġbok ikkuntattja amministratur tas-sistema tiegħek għal aktar informazzjoni.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Jiddispjacini, ċertifikati tas-sigurtà mhumiex attivat għall-kont tiegħek.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Jiddispjacini, is-sistema ċertifikati ta &#39;sigurtà għadha ma ġietx initialized. Jekk jogħġbok ikkuntattja amministratur tas-sistema tiegħek għal aktar informazzjoni.';
+$lang['user_certificates_configuration_file'] = 'konfigurazzjoni File';
+$lang['user_certificates_create_certificates'] = 'Oħloq Ċertifikati';
+$lang['user_certificates_initialize_certificate_help'] = 'Jekk jogħġbok speċifika password għall-protezzjoni ċertifikati ta &#39;sigurtà tiegħek. Jekk tinsa l-password, ser ikollok bżonn biex tibda fuq ma &#39;sett ġdid ta&#39; ċertifikati.';
