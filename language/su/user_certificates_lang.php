c27bec0e6cffa9a2fc07bfb73bc56abd8807d706
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'sertipikat Kaamanan dipaké pikeun ngamankeun rupa aktip nu Anjeun pake dina hiji poé ka dasar poé.';
+$lang['user_certificates_app_name'] = 'Sértipikat pamaké';
+$lang['user_certificates_certificates'] = 'sertipikat';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'sertipikat Pamaké teu sadia pikeun akun ieu.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Hampura, sertipikat kaamanan teu sadia dina sistim ieu. Mangga kontak administrator sistim Anjeun kanggo inpormasi lengkep.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Hampura, sertipikat kaamanan teu diaktipkeun kanggo akun Anjeun.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Hampura, sistim sertipikat kaamanan geus can initialized. Mangga kontak administrator sistim Anjeun kanggo inpormasi lengkep.';
+$lang['user_certificates_configuration_file'] = 'konfigurasi File';
+$lang['user_certificates_create_certificates'] = 'jieun Sértipikat';
+$lang['user_certificates_initialize_certificate_help'] = 'Mangga nangtukeun password pikeun mayungan sertipikat kaamanan Anjeun. Lamun poho password, Anjeun kudu mimitian leuwih jeung susunan anyar sertipikat.';
