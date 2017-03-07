e104b0771968c8f41949e3c27249644c8b23124d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:41:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'وتستخدم الشهادات الأمنية لتأمين مختلف التطبيقات التي تستخدمها على أساس يومي.';
+$lang['user_certificates_app_name'] = 'شهادات المستخدم';
+$lang['user_certificates_certificates'] = 'شهادات';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'شهادات المستخدم لا تتوفر لهذا الحساب.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'عذرا، شهادات الأمان غير متوفرة على هذا النظام. الرجاء الاتصال بمسؤول النظام للحصول على مزيد من المعلومات.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'عذرا، لم يتم تمكين شهادات الأمان لحسابك.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'عذرا، لم يتم تهيئة نظام الشهادات الأمنية. الرجاء الاتصال بمسؤول النظام للحصول على مزيد من المعلومات.';
+$lang['user_certificates_configuration_file'] = 'ملف الضبط';
+$lang['user_certificates_create_certificates'] = 'إنشاء شهادات';
+$lang['user_certificates_initialize_certificate_help'] = 'يرجى تحديد كلمة مرور لحماية شهادات الأمان. إذا كنت قد نسيت كلمة المرور، وسوف تحتاج إلى البدء من جديد مع مجموعة جديدة من الشهادات.';
