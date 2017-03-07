97b209ce451e37be92c146bb123a0a495b87b958
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:53:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Aabo-ẹri ti wa ni lo lati oluso orisirisi lw ti o lo lori a ọjọ lati ọjọ igba.';
+$lang['user_certificates_app_name'] = 'olumulo-ẹri';
+$lang['user_certificates_certificates'] = 'awọn iwe-ẹri';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Olumulo-ẹri wa ni ko wa fun yi iroyin.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Binu, aabo-ẹri ti wa ni ko wa lori yi eto. Jọwọ kan si rẹ eto administrator fun alaye siwaju sii.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Binu, aabo-ẹri ti wa ni ko sise fun àkọọlẹ rẹ.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Binu, awọn aabo-ẹri eto ti ko sibẹsibẹ a initialized. Jọwọ kan si rẹ eto administrator fun alaye siwaju sii.';
+$lang['user_certificates_configuration_file'] = 'iṣeto ni Oluṣakoso';
+$lang['user_certificates_create_certificates'] = 'ṣẹda awọn iwe-ẹri';
+$lang['user_certificates_initialize_certificate_help'] = 'Jọwọ pato kan ọrọigbaniwọle fun idabobo rẹ aabo-ẹri. Ti o ba gbagbe awọn ọrọigbaniwọle, iwọ yoo nilo lati bẹrẹ lori pẹlu titun kan ti ṣeto awọn iwe-ẹri.';
