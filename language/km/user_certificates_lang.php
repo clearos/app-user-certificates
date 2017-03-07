c62316fff674e250ccc4638c4dde322fff3cae71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'វិញ្ញាបនប័ត្រសន្តិសុខត្រូវបានប្រើដើម្បីធានាកម្មវិធីជាច្រើនដែលអ្នកប្រើនៅលើមូលដ្ឋានក្នុងមួយថ្ងៃទៅមួយថ្ងៃ។';
+$lang['user_certificates_app_name'] = 'វិញ្ញាបនប័ត្ររបស់អ្នកប្រើ';
+$lang['user_certificates_certificates'] = 'វិញ្ញាបនប័ត្រ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'វិញ្ញាបនបត្ររបស់អ្នកប្រើគឺមិនអាចរកបានសម្រាប់គណនីនេះ។';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'សូមអភ័យទោសវិញ្ញាបនបត្រសន្តិសុខគឺមិនអាចប្រើបាននៅលើប្រព័ន្ធនេះ។ សូមទាក់ទងអ្នកគ្រប់គ្រងប្រព័ន្ធរបស់អ្នកសម្រាប់បន្ថែម។';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'សូមអភ័យទោសវិញ្ញាបនបត្រសន្តិសុខមិនត្រូវបានបើកសម្រាប់គណនីរបស់អ្នក។';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'សូមអភ័យទោស, ប្រព័ន្ធវិញ្ញាបនបត្រសន្តិសុខបានមិនទាន់ត្រូវបានចាប់ផ្ដើម។ សូមទាក់ទងអ្នកគ្រប់គ្រងប្រព័ន្ធរបស់អ្នកសម្រាប់បន្ថែម។';
+$lang['user_certificates_configuration_file'] = 'ឯកសារកំណត់រចនាសម្ព័ន្ធ';
+$lang['user_certificates_create_certificates'] = 'បង្កើតវិញ្ញាបនបត្រ';
+$lang['user_certificates_initialize_certificate_help'] = 'សូមបញ្ជាក់ពាក្យសម្ងាត់សម្រាប់ការពារវិញ្ញាបនបត្រសុវត្ថិភាពរបស់អ្នក។ ប្រសិនបើអ្នកភ្លេចពាក្យសម្ងាត់នោះអ្នកនឹងត្រូវការដើម្បីចាប់ផ្តើមជាមួយនឹងសំណុំថ្មីមួយនៃវិញ្ញាបនបត្រ។';
