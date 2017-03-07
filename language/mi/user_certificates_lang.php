5a181d9b324dfdcd247c0f737110dcd586356816
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'E whakamahia tiwhikete Haumarutanga ki te mau i ngā taupānga e whakamahi koe i runga i te ra ki te pūtake ra.';
+$lang['user_certificates_app_name'] = 'Tiwhikete kaiwhakamahi';
+$lang['user_certificates_certificates'] = 'tiwhikete';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'e kore e wātea tiwhikete Kaiwhakamahi mo tenei pūkete.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Aroha mai, e kore e wātea ana i runga i tenei pūnaha tiwhikete haumarutanga. Koa whakapā atu ki tō kaiwhakahaere pūnaha mō ētahi atu mōhiohio.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Aroha mai, e kore e whakahohea tiwhikete haumarutanga mo tō pūkete.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Aroha mai, ko te pūnaha tiwhikete haumarutanga kua kore ano i arawhiti. Koa whakapā atu ki tō kaiwhakahaere pūnaha mō ētahi atu mōhiohio.';
+$lang['user_certificates_configuration_file'] = 'Kōnae whirihoranga';
+$lang['user_certificates_create_certificates'] = 'Waihanga Tiwhikete';
+$lang['user_certificates_initialize_certificate_help'] = 'Koa whakapūtā he kupuhipa hoki te tiaki o koutou tiwhikete haumarutanga. Ki te wareware koe te kupuhipa, ka hiahia koe ki te tīmata i runga i ki te huinga hou o tiwhikete.';
