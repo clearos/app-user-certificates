3b1dc9b9cd8bdf39ac7c730e34276f6fa26fde4e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Varnostni certifikati se uporabljajo za pritrditev različne aplikacije, ki jih uporabljajo na vsakodnevno.';
+$lang['user_certificates_app_name'] = 'Uporabniške Potrdila';
+$lang['user_certificates_certificates'] = 'potrdila';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Certifikati uporabnika ni na voljo za ta račun.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Žal, varnostni certifikati niso na voljo v tem sistemu. Obrnite se na skrbnika sistema za več informacij.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Žal, so varnostna spričevala ni omogočen za vaš račun.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Žal, sistem varnostne certifikate še ni inicializiran. Obrnite se na skrbnika sistema za več informacij.';
+$lang['user_certificates_configuration_file'] = 'konfiguracija datoteke';
+$lang['user_certificates_create_certificates'] = 'Ustvari potrdila';
+$lang['user_certificates_initialize_certificate_help'] = 'Navedite geslo za zaščito varnostne certifikate. Če ste pozabili geslo, boste morali začeti znova z novim nizom potrdil.';
