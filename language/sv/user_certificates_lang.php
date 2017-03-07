e62e30fe2af0b65de41250aef9aae0fe5dec795c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Säkerhetscertifikat används för att säkra olika appar som du använder på en daglig basis.';
+$lang['user_certificates_app_name'] = 'Användarcertifikat';
+$lang['user_certificates_certificates'] = 'certifikat';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Användarcertifikat är inte tillgängliga för detta konto.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Tyvärr, säkerhetscertifikat är inte tillgängliga på detta system. Kontakta systemadministratören för mer information.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Tyvärr, är säkerhetscertifikat inte aktiverat för ditt konto.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Tyvärr, säkerhetscertifikat systemet ännu inte har initierats. Kontakta systemadministratören för mer information.';
+$lang['user_certificates_configuration_file'] = 'Inställningsfil';
+$lang['user_certificates_create_certificates'] = 'Skapa certifikat';
+$lang['user_certificates_initialize_certificate_help'] = 'Vänligen ange ett lösenord för att skydda dina säkerhetscertifikat. Om du har glömt lösenordet, måste du börja om med en ny uppsättning av certifikat.';
