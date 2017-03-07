60c12561ef33d08359c74bada2292a37544a5df1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:49:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'सुरक्षा प्रमाणपत्र कि तपाईं दिन को आधार मा एक दिन प्रयोग विभिन्न अनुप्रयोगहरू सुरक्षित गर्न प्रयोग गरिन्छ।';
+$lang['user_certificates_app_name'] = 'प्रयोगकर्ता प्रमाणपत्रहरू';
+$lang['user_certificates_certificates'] = 'प्रमाणपत्र';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'प्रयोगकर्ता प्रमाणपत्र यस खाताको लागि उपलब्ध छैन।';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'माफ गर्नुहोस्, सुरक्षा प्रमाणपत्र यो प्रणाली उपलब्ध छन्। थप जानकारीको लागि कृपया तपाईँको प्रणाली प्रशासकलाई सम्पर्क गर्नुहोस्।';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'माफ गर्नुहोस्, सुरक्षा प्रमाणपत्र तपाईँको खाताको लागि सक्षम छैनन्।';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'माफ गर्नुहोस्, सुरक्षा प्रमाणपत्र सिस्टम अझै आरम्भ गरिएको छैन। थप जानकारीको लागि कृपया तपाईँको प्रणाली प्रशासकलाई सम्पर्क गर्नुहोस्।';
+$lang['user_certificates_configuration_file'] = 'कन्फिगरेसन फाइल';
+$lang['user_certificates_create_certificates'] = 'प्रमाणपत्रहरू सिर्जना';
+$lang['user_certificates_initialize_certificate_help'] = 'कृपया आफ्नो सुरक्षा प्रमाणपत्र सुरक्षा को लागि एक पासवर्ड निर्दिष्ट गर्नुहोस्। तपाईं पासवर्ड बिर्सनुभयो भने, तपाईंले प्रमाणपत्रहरू एक नयाँ सेट सुरु गर्न आवश्यक हुनेछ।';
