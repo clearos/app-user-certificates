2190fe924700ab0431b09a8d0578cd7655d90b39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:43:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Сертификати за сигурност се използват за осигуряване на различни приложения, които се използват за един ден за ден основа.';
+$lang['user_certificates_app_name'] = 'Сертификати на потребителя';
+$lang['user_certificates_certificates'] = 'Сертификати';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Потребителските сертификати не са достъпни за тази сметка.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Съжаляваме, сертификати за сигурност не са на разположение на тази система. Моля, свържете се с вашия системен администратор за повече информация.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Съжаляваме, сертификати за сигурност не са активирани за профила си.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'За съжаление, системата за сертификати за сигурност все още не е инициализиран. Моля, свържете се с вашия системен администратор за повече информация.';
+$lang['user_certificates_configuration_file'] = 'Configuration File';
+$lang['user_certificates_create_certificates'] = 'Създаване Сертификати';
+$lang['user_certificates_initialize_certificate_help'] = 'Моля, посочете парола за защита на вашите сертификати за сигурност. Ако забравите паролата, ще трябва да започнем отначало с нов набор от сертификати.';
