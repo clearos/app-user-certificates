4834e1c6306741cb5dc51501246186bf9772d4f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Drošības sertifikāti tiek izmantoti, lai nodrošinātu dažādas progr, ka jūs izmantojat uz ikdienas pamata.';
+$lang['user_certificates_app_name'] = 'Lietotāja sertifikāti';
+$lang['user_certificates_certificates'] = 'sertifikāti';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Lietotāja sertifikāti nav pieejami šim kontam.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Atvainojiet, drošības sertifikātu nav šajā sistēmā. Lūdzu, sazinieties ar sistēmas administratoru, lai iegūtu vairāk informācijas.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Atvainojiet, drošības sertifikāti nav iespējotas jūsu kontam.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Atvainojiet, drošības sertifikāti sistēma vēl nav inicializēts. Lūdzu, sazinieties ar sistēmas administratoru, lai iegūtu vairāk informācijas.';
+$lang['user_certificates_configuration_file'] = 'Configuration File';
+$lang['user_certificates_create_certificates'] = 'Izveidot sertifikātus';
+$lang['user_certificates_initialize_certificate_help'] = 'Lūdzu, norādiet paroli, drošības sertifikāti aizsardzībai. Ja esat aizmirsis paroli, jums būs nepieciešams, lai sāktu vēlreiz ar jaunu komplektu sertifikātu.';
