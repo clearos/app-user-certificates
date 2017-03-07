8aa680e5305efc36a962a61efdcb0ce84276fa82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:54:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ, ਜੋ ਕਿ ਤੁਹਾਡੇ ਦਿਨ ਨੂੰ ਆਧਾਰ ਕਰਨ ਲਈ ਇੱਕ ਦਿਨ &#39;ਤੇ ਵਰਤਣ ਦੀ ਵੱਖ-ਵੱਖ ਐਪਸ ਨੂੰ ਸੁਰੱਖਿਅਤ ਕਰਨ ਲਈ ਵਰਤਿਆ ਜਾਦਾ ਹੈ.';
+$lang['user_certificates_app_name'] = 'ਯੂਜ਼ਰ ਸਰਟੀਫਿਕੇਟ';
+$lang['user_certificates_certificates'] = 'ਸਰਟੀਫਿਕੇਟ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ਯੂਜ਼ਰ ਸਰਟੀਫਿਕੇਟ ਇਸ ਖਾਤੇ ਲਈ ਉਪਲੱਬਧ ਨਹੀ ਹਨ.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ਅਫਸੋਸ ਹੈ, ਸੁਰੱਖਿਆ ਨੂੰ ਸਰਟੀਫਿਕੇਟ ਇਸ ਸਿਸਟਮ ਤੇ ਉਪਲੱਬਧ ਨਹੀ ਹਨ. ਹੋਰ ਜਾਣਕਾਰੀ ਲਈ ਆਪਣੇ ਸਿਸਟਮ ਪਰਸ਼ਾਸ਼ਕ ਨਾਲ ਸੰਪਰਕ ਕਰੋ ਜੀ.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ਅਫਸੋਸ ਹੈ, ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ ਤੁਹਾਡੇ ਖਾਤੇ ਲਈ ਯੋਗ ਨਾ ਕਰ ਰਹੇ ਹਨ.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ਅਫਸੋਸ ਹੈ, ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ ਸਿਸਟਮ ਨੂੰ ਅਜੇ ਸ਼ੁਰੂ ਨਾ ਕੀਤਾ ਗਿਆ ਹੈ. ਹੋਰ ਜਾਣਕਾਰੀ ਲਈ ਆਪਣੇ ਸਿਸਟਮ ਪਰਸ਼ਾਸ਼ਕ ਨਾਲ ਸੰਪਰਕ ਕਰੋ ਜੀ.';
+$lang['user_certificates_configuration_file'] = 'ਸੰਰਚਨਾ ਫਾਇਲ';
+$lang['user_certificates_create_certificates'] = 'ਸਰਟੀਫਿਕੇਟ ਬਣਾਓ';
+$lang['user_certificates_initialize_certificate_help'] = 'ਆਪਣੀ ਸੁਰੱਖਿਆ ਸਰਟੀਫਿਕੇਟ ਦੀ ਸੁਰੱਖਿਆ ਦੇ ਲਈ ਇੱਕ ਪਾਸਵਰਡ ਦਿਓ ਜੀ. ਤੁਹਾਨੂੰ ਪਾਸਵਰਡ ਭੁੱਲ ਹੋ, ਤੁਹਾਨੂੰ ਸਰਟੀਫਿਕੇਟ ਦੀ ਇੱਕ ਨਵ ਸੈੱਟ ਨਾਲ ਵੱਧ ਸ਼ੁਰੂ ਕਰਨ ਦੀ ਲੋੜ ਹੋਵੇਗੀ.';
