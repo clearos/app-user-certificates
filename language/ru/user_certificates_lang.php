e8d8c62752596993e7b568c13e7710c836629a9b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'сертификаты безопасности используются для защиты различных приложений, которые вы используете на ежедневной основе.';
+$lang['user_certificates_app_name'] = 'Сертификаты пользователя';
+$lang['user_certificates_certificates'] = 'Сертификаты';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Сертификаты пользователя недоступны для этой учетной записи.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'К сожалению, сертификаты безопасности не доступны на этой системе. Пожалуйста, обратитесь к системному администратору для получения дополнительной информации.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'К сожалению, сертификаты безопасности не включена для вашего аккаунта.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'К сожалению, система сертификатов безопасности еще не был инициализирован. Пожалуйста, обратитесь к системному администратору для получения дополнительной информации.';
+$lang['user_certificates_configuration_file'] = 'Файл конфигурации';
+$lang['user_certificates_create_certificates'] = 'Создание сертификатов';
+$lang['user_certificates_initialize_certificate_help'] = 'Укажите пароль для защиты сертификатов безопасности. Если вы забыли пароль, то вам нужно будет начать заново с новым набором сертификатов.';
