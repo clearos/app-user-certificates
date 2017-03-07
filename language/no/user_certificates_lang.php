2b907e57070202af85aec5cf1abd705d16787b98
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Sikkerhetssertifikater brukes til å sikre ulike programmer som du bruker på en dag til dag basis.';
+$lang['user_certificates_app_name'] = 'Brukersertifikater';
+$lang['user_certificates_certificates'] = 'sertifikater';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Brukersertifikater er ikke tilgjengelig for denne kontoen.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Beklager, sikkerhetssertifikater er ikke tilgjengelig på dette systemet. Ta kontakt med systemansvarlig for mer informasjon.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Beklager, sikkerhetssertifikater ikke aktivert for kontoen din.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Beklager, sikkerhetssertifikater systemet ennå ikke er initialisert. Ta kontakt med systemansvarlig for mer informasjon.';
+$lang['user_certificates_configuration_file'] = 'konfigurasjonsfil';
+$lang['user_certificates_create_certificates'] = 'Lag sertifikater';
+$lang['user_certificates_initialize_certificate_help'] = 'Vennligst oppgi et passord for å beskytte dine sikkerhetssertifikater. Hvis du glemmer passordet, må du starte på nytt med et nytt sett med sertifikater.';
