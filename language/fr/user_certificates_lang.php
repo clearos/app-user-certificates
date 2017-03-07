25ea05f891e1763338905d6156388b23a79f39da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:45:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Les certificats de sécurité sont utilisés pour sécuriser diverses applications que vous utilisez sur un jour après jour.';
+$lang['user_certificates_app_name'] = 'Certificats d&#39;utilisateur';
+$lang['user_certificates_certificates'] = 'Certificats';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Les certificats d&#39;utilisateur ne sont pas disponibles pour ce compte.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Désolé, les certificats de sécurité ne sont pas disponibles sur ce système. S&#39;il vous plaît contactez votre administrateur système pour plus d&#39;informations.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Désolé, les certificats de sécurité ne sont pas activés pour votre compte.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Désolé, le système de certificats de sécurité n&#39;a pas encore été initialisé. S&#39;il vous plaît contactez votre administrateur système pour plus d&#39;informations.';
+$lang['user_certificates_configuration_file'] = 'Fichier de configuration';
+$lang['user_certificates_create_certificates'] = 'Créer des certificats';
+$lang['user_certificates_initialize_certificate_help'] = 'S&#39;il vous plaît spécifier un mot de passe pour protéger vos certificats de sécurité. Si vous oubliez le mot de passe, vous devrez recommencer avec une nouvelle série de certificats.';
