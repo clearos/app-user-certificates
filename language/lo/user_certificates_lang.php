49b4dd83e42d81517a058b18ffa0eed1c8f80d8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:51:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ໃບຢັ້ງຢືນຄວາມປອດໄພໄດ້ຖືກນໍາໃຊ້ເພື່ອຮັບປະກັນກິດຕ່າງໆທີ່ທ່ານນໍາໃຊ້ໃນວັນທີ່ຕາມຕໍ່ມື້.';
+$lang['user_certificates_app_name'] = 'ໃບຢັ້ງຢືນຂອງຜູ້ໃຊ້';
+$lang['user_certificates_certificates'] = 'ໃບຢັ້ງຢືນການ';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ໃບຢັ້ງຢືນຂອງຜູ້ໃຊ້ຍັງບໍ່ທັນມີສໍາລັບການບັນຊີນີ້.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ຂໍອະໄພ, ໃບຢັ້ງຢືນຄວາມປອດໄພບໍ່ມີຢູ່ໃນລະບົບນີ້. ກະລຸນາຕິດຕໍ່ຜູ້ບໍລິຫານລະບົບຂອງທ່ານສໍາລັບຂໍ້ມູນເພີ່ມເຕີມ.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ຂໍອະໄພ, ໃບຢັ້ງຢືນຄວາມປອດໄພຍັງບໍ່ໄດ້ເປີດໃຫ້ໃຊ້ງານສໍາລັບບັນຊີຂອງທ່ານ.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ຂໍອະໄພ, ລະບົບໃບຢັ້ງຢືນຄວາມປອດໄພຍັງບໍ່ທັນໄດ້ຮັບການເລີ່ມຕົ້ນ. ກະລຸນາຕິດຕໍ່ຜູ້ບໍລິຫານລະບົບຂອງທ່ານສໍາລັບຂໍ້ມູນເພີ່ມເຕີມ.';
+$lang['user_certificates_configuration_file'] = 'ຕັ້ງຄ່າໄຟລ໌';
+$lang['user_certificates_create_certificates'] = 'ສ້າງໃບຢັ້ງຢືນ';
+$lang['user_certificates_initialize_certificate_help'] = 'ກະລຸນາລະບຸລະຫັດຜ່ານສໍາລັບການປົກປັກຮັກສາໃບຢັ້ງຢືນຄວາມປອດໄພຂອງທ່ານ. ຖ້າຫາກວ່າທ່ານລືມລະຫັດຜ່ານ, ທ່ານຈະຕ້ອງໄດ້ເລີ່ມຕົ້ນໃນໄລຍະທີ່ກໍານົດໄວ້ໃຫມ່ຂອງໃບຢັ້ງຢືນ.';
