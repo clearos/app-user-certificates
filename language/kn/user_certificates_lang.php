d8c518cb7523272553ad28bb17a858945331bad1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:50:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'ಭದ್ರತಾ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ನೀವು ಅರ್ಹರಾಗಿರುತ್ತಾರೆ ಒಂದು ದಿನ ಮೇಲೆ ಬಳಸುವ ವಿವಿಧ Apps ರಕ್ಷಿಸಲು ಬಳಸಲಾಗುತ್ತದೆ.';
+$lang['user_certificates_app_name'] = 'ಬಳಕೆದಾರ ಪ್ರಮಾಣಪತ್ರಗಳು';
+$lang['user_certificates_certificates'] = 'ಪ್ರಮಾಣಪತ್ರಗಳು';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'ಬಳಕೆದಾರ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ಈ ಖಾತೆಗಾಗಿ ಲಭ್ಯವಿಲ್ಲ.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'ಕ್ಷಮಿಸಿ, ಸುರಕ್ಷಿತ ಪ್ರಮಾಣ ಪತ್ರಗಳನ್ನು ಈ ವ್ಯವಸ್ಥೆಯಲ್ಲಿ ಲಭ್ಯವಿದೆ. ದಯವಿಟ್ಟು ಹೆಚ್ಚಿನ ಮಾಹಿತಿಗಾಗಿ ನಿಮ್ಮ ಸಿಸ್ಟಂ ನಿರ್ವಾಹಕರನ್ನು ಸಂಪರ್ಕಿಸಿ.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'ಕ್ಷಮಿಸಿ, ಭದ್ರತಾ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ನಿಮ್ಮ ಖಾತೆಗೆ ಸಕ್ರಿಯಗೊಳಿಸಬೇಕು ಇಲ್ಲ.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'ಕ್ಷಮಿಸಿ, ಭದ್ರತಾ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ವ್ಯವಸ್ಥೆಯು ಈಗಲೂ ಆರಂಭಿಸಲಾಗಿಲ್ಲ ಮಾಡಿಲ್ಲ. ದಯವಿಟ್ಟು ಹೆಚ್ಚಿನ ಮಾಹಿತಿಗಾಗಿ ನಿಮ್ಮ ಸಿಸ್ಟಂ ನಿರ್ವಾಹಕರನ್ನು ಸಂಪರ್ಕಿಸಿ.';
+$lang['user_certificates_configuration_file'] = 'ಸಂರಚನಾ ಕಡತ';
+$lang['user_certificates_create_certificates'] = 'ಪ್ರಮಾಣಪತ್ರಗಳು ರಚಿಸಿ';
+$lang['user_certificates_initialize_certificate_help'] = 'ದಯವಿಟ್ಟು ನಿಮ್ಮ ಭದ್ರತಾ ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ರಕ್ಷಿಸುವ ಪಾಸ್ವರ್ಡ್ ಸೂಚಿಸಿ. ನೀವು ಪಾಸ್ವರ್ಡ್ ಮರೆತುಹೋದಲ್ಲಿ, ನೀವು ಪ್ರಮಾಣಪತ್ರಗಳನ್ನು ಹೊಸ ಜೊತೆ ಪ್ರಾರಂಭಿಸಿ ಮಾಡಬೇಕಾಗುತ್ತದೆ.';
