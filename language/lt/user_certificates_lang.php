572fee1ab9b248966d184fe8147f016af90d3c5d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:48:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Apsaugos sertifikatai naudojami siekiant užtikrinti įvairių programų, kad jūs naudojate ant kasdien.';
+$lang['user_certificates_app_name'] = 'Vartotojo sertifikatai';
+$lang['user_certificates_certificates'] = 'sertifikatai';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Vartotojo sertifikatų nėra šios sąskaitos.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Atsiprašome, saugumo sertifikatų nėra šioje sistemoje. Norėdami gauti daugiau informacijos, susisiekite su sistemos administratoriumi.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Atsiprašome, saugumo sertifikatai nėra įgalinti sąskaitos.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Atsiprašome, Saugumo sertifikatai sistema dar nebuvo inicijuotas. Norėdami gauti daugiau informacijos, susisiekite su sistemos administratoriumi.';
+$lang['user_certificates_configuration_file'] = 'konfigūracijos Failo';
+$lang['user_certificates_create_certificates'] = 'Sukurti sertifikatai';
+$lang['user_certificates_initialize_certificate_help'] = 'Nurodykite slaptažodį apsaugoti savo saugumo sertifikatus. Jei pamiršote savo slaptažodį, jums reikės pradėti iš naujo su nauju sertifikatus.';
