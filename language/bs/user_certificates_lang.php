1fcba5d55b3fb84a8a1b169c0dae13bc780df4ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:42:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Sigurnost certifikati koriste se osigurati razne aplikacije koje koristite na dnevnoj bazi.';
+$lang['user_certificates_app_name'] = 'Potvrde korisnika';
+$lang['user_certificates_certificates'] = 'Potvrde';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'Korisnički sertifikati nisu dostupni za ovaj nalog.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Žao nam je, sigurnosne certifikate nisu dostupne na ovaj sistem. Molimo vas da se obratite administratoru sistema za više informacija.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Žao nam je, certifikati sigurnosti nisu omogućene za svoj račun.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Nažalost, ovlaąćenja sistem još nije inicijaliziran. Molimo vas da se obratite administratoru sistema za više informacija.';
+$lang['user_certificates_configuration_file'] = 'Configuration File';
+$lang['user_certificates_create_certificates'] = 'Napravi Potvrde';
+$lang['user_certificates_initialize_certificate_help'] = 'Navedite lozinku za zaštitu vaše sigurnosti certifikate. Ako ste zaboravili lozinku, morat ćete početi ispočetka s novim setom certifikata.';
