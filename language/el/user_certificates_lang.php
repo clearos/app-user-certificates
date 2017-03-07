33fbb4f465e9ec902f9fa6590bfe542b066642be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:46:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Τα πιστοποιητικά ασφαλείας που χρησιμοποιούνται για την εξασφάλιση διάφορες εφαρμογές που χρησιμοποιείτε σε καθημερινή βάση.';
+$lang['user_certificates_app_name'] = 'πιστοποιητικά χρήστη';
+$lang['user_certificates_certificates'] = 'πιστοποιητικά';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'πιστοποιητικά χρήστη δεν είναι διαθέσιμες για αυτόν το λογαριασμό.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Συγνώμη, πιστοποιητικά ασφαλείας δεν είναι διαθέσιμες σε αυτό το σύστημα. Παρακαλώ επικοινωνήστε με το διαχειριστή του συστήματός σας για περισσότερες πληροφορίες.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Συγγνώμη, τα πιστοποιητικά ασφαλείας δεν είναι ενεργοποιημένη για το λογαριασμό σας.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Συγνώμη, το σύστημα των πιστοποιητικών ασφαλείας δεν έχει ακόμη προετοιμαστεί. Παρακαλώ επικοινωνήστε με το διαχειριστή του συστήματός σας για περισσότερες πληροφορίες.';
+$lang['user_certificates_configuration_file'] = 'διαμόρφωση αρχείου';
+$lang['user_certificates_create_certificates'] = 'Δημιουργία πιστοποιητικά';
+$lang['user_certificates_initialize_certificate_help'] = 'Παρακαλείστε να προσδιορίσετε έναν κωδικό πρόσβασης για την προστασία των πιστοποιητικών ασφαλείας σας. Εάν ξεχάσετε τον κωδικό πρόσβασης, θα πρέπει να ξεκινήσετε από την αρχή με ένα νέο σύνολο των πιστοποιητικών.';
