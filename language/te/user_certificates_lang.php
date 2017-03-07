1cfe8f69650a550c170c644f94e403923fbe14c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:52:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'సెక్యూరిటీ సర్టిఫికెట్లు మీరు రోజు ఆధారంగా ఒక రోజు ఉపయోగించే వివిధ అనువర్తనాల సురక్షిత ఉపయోగిస్తారు.';
+$lang['user_certificates_app_name'] = 'వాడుకరి సర్టిఫికెట్లు';
+$lang['user_certificates_certificates'] = 'సర్టిఫికెట్లు';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'వాడుకరి ధృవపత్రాలు ఈ ఖాతా కోసం అందుబాటులో లేవు.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'క్షమించాలి, భద్రత సర్టిఫికేట్లు ఈ వ్యవస్థ లో అందుబాటులో ఉన్నాయి. దయచేసి మరింత సమాచారం కోసం మీ సిస్టమ్ నిర్వాహకుడిని సంప్రదించండి.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'క్షమించాలి, భద్రత సర్టిఫికేట్లు మీ ఖాతా కోసం ఎనేబుల్ లేదు.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'క్షమించాలి, భద్రత సర్టిఫికేట్లు విధానం ఇంకా initialized లేదు. దయచేసి మరింత సమాచారం కోసం మీ సిస్టమ్ నిర్వాహకుడిని సంప్రదించండి.';
+$lang['user_certificates_configuration_file'] = 'ఆకృతీకరణ ఫైలు';
+$lang['user_certificates_create_certificates'] = 'సర్టిఫికెట్లు సృష్టించు';
+$lang['user_certificates_initialize_certificate_help'] = 'దయచేసి మీ భద్రత సర్టిఫికేట్లు రక్షించటం పాస్వర్డ్ను పేర్కొనండి. మీరు పాస్వర్డ్ను మర్చిపోతే, మీరు సర్టిఫికేట్లలో ఒక కొత్త సెట్ తో ప్రారంభించడానికి అవసరం.';
