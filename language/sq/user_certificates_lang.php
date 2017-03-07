5732393c63a9fd5a5d5e92509652edfe7e6dc3be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-07 00:40:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/user_certificates_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['user_certificates_app_description'] = 'Certifikatat e sigurisë janë përdorur për të siguruar aplikacione të ndryshme që ju përdorni në një ditë në baza ditore.';
+$lang['user_certificates_app_name'] = 'Certifikata e përdoruesit';
+$lang['user_certificates_certificates'] = 'Certifikata';
+$lang['user_certificates_certificates_not_available_for_this_account'] = 'certifikatat e përdoruesit nuk janë në dispozicion për këtë llogari.';
+$lang['user_certificates_certificates_not_available_on_this_system'] = 'Na vjen keq, certifikatat e sigurisë nuk janë në dispozicion në këtë sistem. Ju lutemi të kontaktoni administratorin e sistemit tuaj për më shumë informacion.';
+$lang['user_certificates_certificates_not_enabled_for_your_account'] = 'Na vjen keq, certifikatat e sigurisë nuk janë të aktivizuara për llogarinë tuaj.';
+$lang['user_certificates_certificates_not_yet_initialized'] = 'Na vjen keq, sistemi certifikatat e sigurisë ende nuk është nisur. Ju lutemi të kontaktoni administratorin e sistemit tuaj për më shumë informacion.';
+$lang['user_certificates_configuration_file'] = 'Configuration File';
+$lang['user_certificates_create_certificates'] = 'Krijo Certifikata';
+$lang['user_certificates_initialize_certificate_help'] = 'Ju lutem specifikoni një fjalëkalim për mbrojtjen certifikatat e sigurisë. Nëse ju harroni fjalëkalimin, ju do të duhet të fillojë gjatë me një seri të re të certifikatave.';
